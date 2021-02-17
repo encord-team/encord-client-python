d692bb0c0226df1f2f11af385cc586c4b5def46f
Ulrik Stig Hansen <ulle2tm@gmail.com>
2021-02-17 13:27:29 -0800

    updated label utils

--- cord/utils/str_constants.py
+++ cord/utils/str_constants.py
@@ -1,6 +1,12 @@
+# Label row constants
+LABELS = "labels"
 OBJECTS = "objects"
 CLASSIFICATIONS = "classifications"
 OBJECT_HASH = "objectHash"
 CLASSIFICATION_HASH = "classificationHash"
 OBJECT_ANSWERS = "object_answers"
 CLASSIFICATION_ANSWERS = "classification_answers"
+
+# Data types
+VIDEO = "video"
+IMG_GROUP = "img_group"
