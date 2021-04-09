7230710a4d4c5f5ca74d05577138efe3e2c35d1a
Ulrik Stig Hansen <ulle2tm@gmail.com>
2021-04-09 13:40:44 +0100

    updated release number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cord-client-python",
-    version="0.0.9",
+    version="0.1.0",
     author="Cord Technologies Limited",
     author_email="hello@cord.tech",
     description="Cord Python API Client",
