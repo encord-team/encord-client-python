44eb77e3db5f2b9b6f3c2e8036b2dcfa404e40ab
Ulrik Stig Hansen <ulle2tm@gmail.com>
2021-06-24 17:30:07 +0100

    updated to appropriate license

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        'License :: OSI Approved :: Apache Software License',
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
