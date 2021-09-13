f55859542367a5be1211be169ecd8aaabef5f668
Ramnik Jain <ramnik@cord.tech>
2021-09-13 02:02:26 +0100

    Client method to Bulk Delete Video/Image Group

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cord-client-python",
-    version="0.1.11",
+    version="0.1.13",
     author="Cord Technologies Limited",
     author_email="hello@cord.tech",
     description="Cord Python API Client",
