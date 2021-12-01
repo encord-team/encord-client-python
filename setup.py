0b7f065938b6032b14ec33e41adb74a6a2afaf21
Ramnik Jain <ramnik@cord.tech>
2021-12-01 19:22:07 +0000

    Version Bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cord-client-python",
-    version="0.1.20",
+    version="0.1.21",
     author="Cord Technologies Limited",
     author_email="hello@cord.tech",
     description="Cord Python API Client",
