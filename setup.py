8d666e1750ab7567649bab89fe39e80b9a19c7f1
Ulrik Stig Hansen <ulle2tm@gmail.com>
2022-01-10 17:11:39 +0000

    version bump to 0.1.23

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cord-client-python",
-    version="0.1.22",
+    version="0.1.23",
     author="Cord Technologies Limited",
     author_email="hello@cord.tech",
     description="Cord Python API Client",
