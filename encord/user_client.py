198166a8e7dd298e6f646097bf2821bdd8a2965e
Alexey Kirichun <alexey@encord.com>
2022-02-14 21:57:52 +0000

    More user friendly filtering (if a bit hackily implemented)

--- encord/user_client.py
+++ encord/user_client.py
@@ -45,22 +45,24 @@ class EncordUserClient:
         self.user_config = user_config
         self.querier = querier
 
+
     def create_private_dataset(
-        self,
-        dataset_title: str,
-        dataset_type: StorageLocation,
-        dataset_description: Optional[str] = None,
+            self,
+            dataset_title: str,
+            dataset_type: StorageLocation,
+            dataset_description: Optional[str] = None,
     ) -> CreateDatasetResponse:
         """
         DEPRECATED - please use `create_dataset` instead.
         """
         return self.create_dataset(dataset_title, dataset_type, dataset_description)
 
+
     def create_dataset(
-        self,
-        dataset_title: str,
-        dataset_type: StorageLocation,
-        dataset_description: Optional[str] = None,
+            self,
+            dataset_title: str,
+            dataset_type: StorageLocation,
+            dataset_description: Optional[str] = None,
     ) -> CreateDatasetResponse:
         """
         Args:
@@ -84,8 +86,9 @@ class EncordUserClient:
         result = self.querier.basic_setter(Dataset, uid=None, payload=dataset)
         return CreateDatasetResponse.from_dict(result)
 
+
     def create_dataset_api_key(
-        self, dataset_hash: str, api_key_title: str, dataset_scopes: List[DatasetScope]
+            self, dataset_hash: str, api_key_title: str, dataset_scopes: List[DatasetScope]
     ) -> DatasetAPIKey:
         api_key_payload = {
             "dataset_hash": dataset_hash,
@@ -95,12 +98,14 @@ class EncordUserClient:
         response = self.querier.basic_setter(DatasetAPIKey, uid=None, payload=api_key_payload)
         return DatasetAPIKey.from_dict(response)
 
+
     def get_dataset_api_keys(self, dataset_hash: str) -> List[DatasetAPIKey]:
         api_key_payload = {
             "dataset_hash": dataset_hash,
         }
         api_keys: List[DatasetAPIKey] = self.querier.get_multiple(DatasetAPIKey, uid=None, payload=api_key_payload)
         return api_keys
+
 
     def get_or_create_dataset_api_key(self, dataset_hash: str) -> DatasetAPIKey:
         api_key_payload = {
@@ -109,11 +114,39 @@ class EncordUserClient:
         response = self.querier.basic_put(DatasetAPIKey, uid=None, payload=api_key_payload)
         return DatasetAPIKey.from_dict(response)
 
-    def get_datasets(self, properties_filter: Optional[Dict] = None) -> List[Dict]:
-        if properties_filter is not None:
-            properties_filter = self.__validate_filter(properties_filter)
+
+    def get_datasets(
+            self,
+            title_eq: Optional[str] = None,
+            title_like: Optional[str] = None,
+            desc_eq: Optional[str] = None,
+            desc_like: Optional[str] = None,
+            created_before: Optional[Union[str, datetime]] = None,
+            created_after: Optional[Union[str, datetime]] = None,
+            edited_before: Optional[Union[str, datetime]] = None,
+            edited_after: Optional[Union[str, datetime]] = None,
+    ) -> List[Dict]:
+        """
+        List either all (if called with no arguments) or matching datasets the user has access to.
+
+        Args:
+            title_eq: optional exact title filter
+            title_like: optional fuzzy title filter; SQL syntax
+            desc_eq: optional exact description filter
+            desc_like: optional fuzzy description filter; SQL syntax
+            created_before: optional creation date filter, 'less'
+            created_after: optional creation date filter, 'greater'
+            edited_before: optional last modification date filter, 'less'
+            edited_after: optional last modification date filter, 'greater'
+
+        Returns:
+            list of (role, dataset) pairs for datasets  matching filter conditions.
+        """
+        properties_filter = self.__validate_filter(locals())
+        # a hack to be able to share validation code without too much c&p
         data = self.querier.get_multiple(DatasetWithUserRole, payload={'filter': properties_filter})
         return [{"dataset": DatasetInfo(**d.dataset), "user_role": DatasetUserRole(d.user_role)} for d in data]
+
 
     @staticmethod
     def create_with_ssh_private_key(ssh_private_key: str, password: str = None, **kwargs) -> EncordUserClient:
@@ -122,16 +155,45 @@ class EncordUserClient:
 
         return EncordUserClient(user_config, querier)
 
-    def get_projects(self, properties_filter: Optional[Dict] = None):
-        if properties_filter is not None:
-            properties_filter = self.__validate_filter(properties_filter)
+
+    def get_projects(
+            self,
+            title_eq: Optional[str] = None,
+            title_like: Optional[str] = None,
+            desc_eq: Optional[str] = None,
+            desc_like: Optional[str] = None,
+            created_before: Optional[Union[str, datetime]] = None,
+            created_after: Optional[Union[str, datetime]] = None,
+            edited_before: Optional[Union[str, datetime]] = None,
+            edited_after: Optional[Union[str, datetime]] = None,
+    ) -> List[Dict]:
+        """
+        List either all (if called with no arguments) or matching projects the user has access to.
+
+        Args:
+            title_eq: optional exact title filter
+            title_like: optional fuzzy title filter; SQL syntax
+            desc_eq: optional exact description filter
+            desc_like: optional fuzzy description filter; SQL syntax
+            created_before: optional creation date filter, 'less'
+            created_after: optional creation date filter, 'greater'
+            edited_before: optional last modification date filter, 'less'
+            edited_after: optional last modification date filter, 'greater'
+
+        Returns:
+            list of (role, projects) pairs for project matching filter conditions.
+        """
+        properties_filter = self.__validate_filter(locals())
+        # a hack to be able to share validation code without too much c&p
         data = self.querier.get_multiple(ProjectWithUserRole, payload={'filter': properties_filter})
         return [{"project": Project(p.project), "user_role": ProjectUserRole(p.user_role)} for p in data]
+
 
     def create_project(self, project_title: str, dataset_hashes: List[str], project_description: str = "") -> str:
         project = {"title": project_title, "description": project_description, "dataset_hashes": dataset_hashes}
 
         return self.querier.basic_setter(Project, uid=None, payload=project)
+
 
     def create_project_api_key(self, project_hash: str, api_key_title: str, scopes: List[APIKeyScopes]) -> str:
         """
@@ -142,26 +204,31 @@ class EncordUserClient:
 
         return self.querier.basic_setter(ProjectAPIKey, uid=project_hash, payload=payload)
 
+
     def get_project_api_keys(self, project_hash: str) -> List[ProjectAPIKey]:
         return self.querier.get_multiple(ProjectAPIKey, uid=project_hash)
 
+
     def get_or_create_project_api_key(self, project_hash: str) -> str:
         return self.querier.basic_put(ProjectAPIKey, uid=project_hash, payload={})
+
 
     def get_dataset_client(self, dataset_hash: str, **kwargs) -> Union[EncordClientProject, EncordClientDataset]:
         dataset_api_key: DatasetAPIKey = self.get_or_create_dataset_api_key(dataset_hash)
         return EncordClient.initialise(dataset_hash, dataset_api_key.api_key, **kwargs)
 
+
     def get_project_client(self, project_hash: str, **kwargs) -> Union[EncordClientProject, EncordClientDataset]:
         project_api_key: str = self.get_or_create_project_api_key(project_hash)
         return EncordClient.initialise(project_hash, project_api_key, **kwargs)
 
+
     def create_project_from_cvat(
-        self,
-        import_method: ImportMethod,
-        dataset_name: str,
-        review_mode: ReviewMode = ReviewMode.LABELLED,
-        max_workers: Optional[int] = None,
+            self,
+            import_method: ImportMethod,
+            dataset_name: str,
+            review_mode: ReviewMode = ReviewMode.LABELLED,
+            max_workers: Optional[int] = None,
     ) -> Union[CvatImporterSuccess, CvatImporterError]:
         """
         Export your CVAT project with the "CVAT for images 1.1" option and use this function to import
@@ -235,6 +302,7 @@ class EncordUserClient:
         else:
             raise ValueError("The api server responded with an invalid payload.")
 
+
     def __get_images_paths(self, annotations_base64: str, images_directory_path: Path) -> List[Path]:
         payload = {"annotations_base64": annotations_base64}
         project_info = self.querier.basic_setter(ProjectImporterCvatInfo, uid=None, payload=payload)
@@ -260,8 +328,9 @@ class EncordUserClient:
             raise ValueError(f"No images found in the provided data folder.")
         return images
 
+
     def __upload_cvat_images(
-        self, images_paths: List[Path], dataset_name: str, max_workers: int
+            self, images_paths: List[Path], dataset_name: str, max_workers: int
     ) -> Tuple[str, Dict[str, str]]:
         """
         This function does not create any image groups yet.
@@ -293,8 +362,10 @@ class EncordUserClient:
 
         return dataset_hash, image_title_to_image_hash_map
 
+
     def get_cloud_integrations(self) -> List[CloudIntegration]:
         return self.querier.get_multiple(CloudIntegration)
+
 
     def __validate_filter(self, properties_filter: Dict) -> Dict:
         if not isinstance(properties_filter, dict):
@@ -306,11 +377,14 @@ class EncordUserClient:
 
         # be relaxed with what we receive: translate raw strings to enum values
         for (clause, val) in properties_filter.items():
+            if val is None:
+                continue
+
             if isinstance(clause, str) and clause in valid_filters:
                 clause = ListingFilter(clause)
 
             if clause not in ListingFilter:
-                raise ValueError(f"Unknown filter '{clause}")
+                continue
 
             if clause.name.endswith("before") or clause.name.endswith("after"):
                 if isinstance(val, datetime.datetime):
