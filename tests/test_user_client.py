eadffd2506d29aaf25ea98f983a37dcb43eadc6e
Frederik Hvilshøj <frederik@cord.tech>
2022-05-18 12:17:30 +0200

    Teardown for tests.

--- tests/test_user_client.py
+++ tests/test_user_client.py
@@ -1,28 +1,39 @@
 import os
 
 import pytest
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
 
 import encord.exceptions
 from encord.configs import _ENCORD_SSH_KEY, _ENCORD_SSH_KEY_FILE
 from encord.user_client import EncordUserClient
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
 
-PRIVATE_KEY = Ed25519PrivateKey.generate().private_bytes(
-    encoding=serialization.Encoding.PEM,
-    format=serialization.PrivateFormat.OpenSSH,
-    encryption_algorithm=serialization.NoEncryption()
-).decode('utf-8')
+PRIVATE_KEY = (
+    Ed25519PrivateKey.generate()
+    .private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.OpenSSH,
+        encryption_algorithm=serialization.NoEncryption(),
+    )
+    .decode("utf-8")
+)
 
 
 @pytest.fixture
 def ssh_key_file_path():
-    return "./resources/test_key"
+    return os.path.join(os.path.dirname(__file__), "resources/test_key")
 
 
 @pytest.fixture
 def ssh_key_content():
     return PRIVATE_KEY
+
+
+def teardown_function():
+    if _ENCORD_SSH_KEY_FILE in os.environ:
+        del os.environ[_ENCORD_SSH_KEY_FILE]
+    if _ENCORD_SSH_KEY in os.environ:
+        del os.environ[_ENCORD_SSH_KEY]
 
 
 def test_initialise_without_env_variables_or_arguments():
@@ -50,6 +61,7 @@ def test_initialise_with_correct_ssh_file_content(ssh_key_content):
 
 
 def test_initialise_with_correct_ssh_file_content_from_env(ssh_key_content):
+    assert _ENCORD_SSH_KEY_FILE not in os.environ
     os.environ[_ENCORD_SSH_KEY] = ssh_key_content
     user_client = EncordUserClient.create_with_ssh_private_key()
     assert isinstance(user_client, EncordUserClient)
@@ -61,7 +73,7 @@ def test_initialise_with_wrong_ssh_file_content():
 
 
 def test_initialise_with_wrong_ssh_file_content_from_env():
+    assert _ENCORD_SSH_KEY_FILE not in os.environ
     os.environ[_ENCORD_SSH_KEY] = "Some random content."
     with pytest.raises(expected_exception=ValueError) as excinfo:
         user_client = EncordUserClient.create_with_ssh_private_key()
-
