8d48958f3888d407262a83a85a0800b62d18572c
Frederik Hvilshøj <frederik@cord.tech>
2022-07-11 11:28:27 +0200

    [Docs] Black for code examples with pyproject.yaml config file (now build_docs follow pre-commit)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,6 @@
 # -- Path setup --------------------------------------------------------------
 
 import json
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
