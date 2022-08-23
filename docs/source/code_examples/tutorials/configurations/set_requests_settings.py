a617e6a52a63ce0caf89822c94df99d1934128f3
denis-cord <denis@cord.tech>
2022-08-23 12:25:37 +0100

    Remove backoff factor

--- docs/source/code_examples/tutorials/configurations/set_requests_settings.py
+++ docs/source/code_examples/tutorials/configurations/set_requests_settings.py
@@ -3,5 +3,5 @@ from encord.http.constants import RequestsSettings
 
 user_client: EncordUserClient = EncordUserClient.create_with_ssh_private_key(
     "<your_private_key>",
-    requests_settings=RequestsSettings(max_retries=7, backoff_factor=0.3),
+    requests_settings=RequestsSettings(max_retries=7),
 )
