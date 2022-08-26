7bc2badc2842fbe6d1279d110a51af2cfae9fe0e
denis-cord <denis@cord.tech>
2022-08-26 17:06:07 +0100

    Clean up integration tests

--- tests/transformers/coco/integration/test_coco_transcoder.py
+++ tests/transformers/coco/integration/test_coco_transcoder.py
@@ -11,9 +11,9 @@ from encord.transformers.coco.coco_encoder import CocoEncoder
 
 DEV_DOMAIN = "https://dev.api.cord.tech"
 LOCAL_DOMAIN = "http://127.0.0.1:8000"
-STAGING_DOMAIN = "https://staging.api.cord.tech"
+STAGING_DOMAIN = "https://staging.api.encord.com"
 
-USED_DOMAIN = LOCAL_DOMAIN
+USED_DOMAIN = STAGING_DOMAIN
 
 # PROJECT_RESOURCE_ID = "11cb4f95-7483-4867-b0c4-0a0fb23464a6"  # Task with 3 PNG
 # PROJECT_RESOURCE_ID = "37d47b8d-c5d9-46ef-8591-5190a4f932f3"  # Polyline example on local dev
@@ -21,7 +21,9 @@ USED_DOMAIN = LOCAL_DOMAIN
 # PROJECT_RESOURCE_ID = "1a0a2e01-9d75-4781-95c1-0e2efe0d31e9"  # dicom project on staging
 # PROJECT_RESOURCE_ID = "73822617-4183-4202-99e9-3bbfd0f9f0ee"  # Inference and training project on local dev
 # PROJECT_RESOURCE_ID = "ee79c249-8b84-4d4c-8528-d73cab7a1ae3"  # Project with many images dataset on local dev
-PROJECT_RESOURCE_ID = "3bdd05f0-da72-49ec-99b5-4770ecdcfb55"  # Project for COCO exporting on local dev
+# PROJECT_RESOURCE_ID = "3bdd05f0-da72-49ec-99b5-4770ecdcfb55"  # Project for COCO exporting on local dev
+PROJECT_RESOURCE_ID = "988825c5-b566-443e-9c09-95cf6c5f4d0d"  # COCO demo - staging
+
 
 # DATASET_RESOURCE_ID = "f56be19d-4202-46a2-8f40-5e2eef649ffe"  # CVAT dataset
 # DATASET_RESOURCE_ID = "d7cbfc0e-9fe5-4b55-af5e-773e2e3a0bc4"  # "Video Dataset" local dev
@@ -62,7 +64,7 @@ def test_run_torch_full_integration():
     project = get_project_ssh()
     labels = []
     for label_row in project.label_rows:
-        if label_row["label_hash"] is not None and label_row["data_title"] == "custom image group SDK":
+        if label_row["label_hash"] is not None:
             labels.append(project.get_label_row(label_row["label_hash"]))
 
     ontology = OntologyStructure.from_dict(project.ontology)
@@ -73,21 +75,15 @@ def test_run_torch_full_integration():
         include_videos=True,
         include_unannotated_videos=False,
     )
+    print(coco_format)
     with open("data/full_test/coco.json", "w") as f:
         json.dump(coco_format, f)
 
     torch_output = torchvision.datasets.CocoDetection("data/full_test", "data/full_test/coco.json")
     print(len(torch_output))
 
-    # img, target = next(iter(torch_output))
-    # print("hello")
     import matplotlib.pyplot as pyplot_
 
-    #
-    # pyplot_.imshow(img)
-    # torch_output.coco.showAnns(target)
-    # pyplot_.show()
-    # print("")
     for i in torch_output:
         img, target = i
         pyplot_.imshow(img)
