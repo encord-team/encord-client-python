d5cb63cecc59f5de69662515c342e6451f72788c
denis-cord <94055292+denis-cord@users.noreply.github.com>
2022-10-21 19:16:36 +0100

    Update CVAT docs around requests timeouts (#208)

--- docs/source/code_examples/tutorials/configurations/overwrite_timeouts.py
+++ docs/source/code_examples/tutorials/configurations/overwrite_timeouts.py
@@ -7,6 +7,12 @@ user_client: EncordUserClient = EncordUserClient.create_with_ssh_private_key(
 
 NEW_TIMEOUT = 300  # seconds
 
+# For user_client requests specifically. These will NOT automatically
+# propagate to the Dataset or Project objects
+user_client.user_config.read_timeout = NEW_TIMEOUT
+user_client.user_config.write_timeout = NEW_TIMEOUT
+user_client.user_config.connect_timeout = NEW_TIMEOUT
+
 # The same procedure works for the Project class that is returned from
 # `user_client.get_project("<project_hash>")
 dataset: Dataset = user_client.get_dataset("<dataset_hash>")
