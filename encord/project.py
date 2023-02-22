472f462df401fbc5d81ab2825561b07ce9b5fecb
denis-cord <94055292+denis-cord@users.noreply.github.com>
2023-02-22 09:49:04 +0000

    Add more filters to list_label_rows; Add more data to LabelRowMetadata (#246)
    This will then be the preferred method of getting label rows. This over using project.label_rows.

--- encord/project.py
+++ encord/project.py
@@ -147,6 +147,10 @@ class Project:
         edited_after: Optional[Union[str, datetime.datetime]] = None,
         label_statuses: Optional[List[AnnotationTaskStatus]] = None,
         shadow_data_state: Optional[ShadowDataState] = None,
+        *,
+        include_uninitialised_labels=False,
+        label_hashes: Optional[List[str]] = None,
+        data_hashes: Optional[List[str]] = None,
     ) -> List[LabelRowMetadata]:
         """
         Args:
@@ -155,6 +159,11 @@ class Project:
             edited_after: Optionally filter to only rows last edited after the specified time
             label_statuses: Optionally filter to only those label rows that have one of the specified :class:`~encord.orm.label_row.AnnotationTaskStatus`es
             shadow_data_state: On Optionally filter by data type in Benchmark QA projects. See :class:`~encord.orm.label_row.ShadowDataState`
+            include_uninitialised_labels: Whether to return only label rows that are "created" and have a label_hash
+                (default). If set to `True`, this will return all label rows, including those that do not have a
+                label_hash.
+            data_hashes: List of data hashes to filter by.
+            label_hashes: List of label hashes to filter by.
 
         Returns:
             A list of :class:`~encord.orm.label_row.LabelRowMetadata` instances for all the matching label rows
@@ -163,7 +172,15 @@ class Project:
             AuthorisationError: If not a member of the project.
             UnknownError: If an error occurs while retrieving the data.
         """
-        return self._client.list_label_rows(edited_before, edited_after, label_statuses, shadow_data_state)
+        return self._client.list_label_rows(
+            edited_before,
+            edited_after,
+            label_statuses,
+            shadow_data_state,
+            include_uninitialised_labels=include_uninitialised_labels,
+            label_hashes=label_hashes,
+            data_hashes=data_hashes,
+        )
 
     def set_label_status(self, label_hash: str, label_status: LabelStatus) -> bool:
         """
