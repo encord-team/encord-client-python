27821353321dba072ce2415cfda684c12e2256f9
david-babuschkin-encord <david.babuschkin@encord.com>
2023-04-27 16:15:09 +0100

    changed style

--- docs/source/code_examples/tutorials/ontologies/copying_ontology.py
+++ docs/source/code_examples/tutorials/ontologies/copying_ontology.py
@@ -4,9 +4,9 @@ user_client: EncordUserClient = EncordUserClient.create_with_ssh_private_key(
     "<your_private_key>"
 )
 
-ontology = user_client.get_ontology('<ontology_hash>')
+ontology = user_client.get_ontology("<ontology_hash>")
 new_ontology = user_client.create_ontology(
-    'copy ontology', 
-    description='<my ontology description>', 
+    "copy ontology", 
+    description="<my ontology description>", 
     structure=ontology.structure,
 )
