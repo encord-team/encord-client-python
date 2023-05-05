261d5ac541a03860cec1244f7903281b75b91ebf
Sergei Chertkov <sergei@encord.com>
2023-05-05 14:26:01 +0100

    Correct documentation

--- encord/objects/ontology_labels_impl.py
+++ encord/objects/ontology_labels_impl.py
@@ -915,9 +915,9 @@ class LabelRowV2:
     @property
     def label_status(self) -> LabelStatus:
         """
-        Returns current labeling status for the label row.
+        Returns the current labeling status for the label row.
 
-        **Note**: is not supported for the workflow-based projects. See the documentation about workflows
+        **Note**: This method is not supported for workflow-based projects. Please see our workflow documentation for more details.
         """
 
         if self.__is_tms2_project:
@@ -931,9 +931,9 @@ class LabelRowV2:
     @property
     def annotation_task_status(self) -> AnnotationTaskStatus:
         """
-        Returns current annotation task status for the label row.
+        Returns the current annotation task status for the label row.
 
-        **Note**: is not supported for the workflow-based projects. See the documentation about workflows
+        **Note**: This method is not supported for workflow-based projects. Please see our workflow documentation for more details.
         """
         if self.__is_tms2_project:
             raise WrongProjectTypeError(
