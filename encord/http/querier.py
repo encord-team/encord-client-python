76512ba766260450188490944865b1bed37e22c6
Sergei Chertkov <sergei@encord.com>
2023-05-16 10:49:37 +0100

    Add cloud trace id generation

--- encord/http/querier.py
+++ encord/http/querier.py
@@ -15,12 +15,14 @@
 import dataclasses
 import logging
 import platform
+import uuid
+import random
 from contextlib import contextmanager
-from typing import Any, List, Optional, Type, TypeVar, Generator
+from typing import Any, List, Tuple, Optional, Type, TypeVar, Generator
 
 import requests
 import requests.exceptions
-from requests import Session
+from requests import Session, Response
 from requests.adapters import HTTPAdapter
 from urllib3 import Retry
 
@@ -30,10 +32,18 @@ from encord.http.error_utils import check_error_response
 from encord.http.query_methods import QueryMethods
 from encord.http.request import Request
 from encord.orm.formatter import Formatter
-from encord.orm.base_orm import BaseORM
 from encord._version import __version__ as encord_version
 
 logger = logging.getLogger(__name__)
+
+HEADER_USER_AGENT = "User-Agent"
+HEADER_CLOUD_TRACE_CONTEXT = "X-Cloud-Trace-Context"
+
+
+@dataclass
+class RequestContext(ExceptionContext):
+    trace_id: Optional[str] = None
+    span_id: Optional[str] = None
 
 
 class Querier:
@@ -47,11 +57,11 @@ class Querier:
     def basic_getter(self, db_object_type: Type[T], uid=None, payload=None) -> T:
         """Single DB object getter."""
         request = self._request(QueryMethods.GET, db_object_type, uid, self._config.read_timeout, payload=payload)
-        res = self._execute(request)
+        res, context = self._execute(request)
         if res:
             return self._parse_response(db_object_type, res)
         else:
-            raise ResourceNotFoundError("Resource not found.")
+            raise ResourceNotFoundError("Resource not found.", context=context)
 
     def get_multiple(self, object_type: Type[T], uid=None, payload=None) -> List[T]:
         return self._request_multiple(QueryMethods.GET, object_type, uid, payload)
@@ -64,12 +74,14 @@ class Querier:
 
     def _request_multiple(self, method: QueryMethods, object_type: Type[T], uid: List[str], payload=None) -> List[T]:
         request = self._request(method, object_type, uid, self._config.read_timeout, payload=payload)
-        result = self._execute(request)
+        result, context = self._execute(request)
 
         if result is not None:
             return [self._parse_response(object_type, item) for item in result]
         else:
-            raise ResourceNotFoundError(f"[{object_type}] not found for query with uid=[{uid}] and payload=[{payload}]")
+            raise ResourceNotFoundError(
+                f"[{object_type}] not found for query with uid=[{uid}] and payload=[{payload}]", context=context
+            )
 
     @staticmethod
     def _parse_response(object_type: Type[T], item: dict) -> T:
@@ -89,7 +101,8 @@ class Querier:
             self._config.read_timeout,
         )
 
-        return self._execute(request)
+        res, _ = self._execute(request)
+        return res
 
     def basic_setter(self, db_object_type: Type[T], uid, payload):
         """Single DB object setter."""
@@ -101,12 +114,12 @@ class Querier:
             payload=payload,
         )
 
-        res = self._execute(request)
+        res, context = self._execute(request)
 
         if res:
             return res
         else:
-            raise RequestException("Setting %s with uid %s failed." % (db_object_type, uid))
+            raise RequestException(f"Setting {db_object_type} with uid {uid} failed.", context=context)
 
     def basic_put(self, db_object_type, uid, payload, enable_logging=True):
         """Single DB object put request."""
@@ -118,26 +131,44 @@ class Querier:
             payload=payload,
         )
 
-        res = self._execute(request, enable_logging)
+        res, context = self._execute(request, enable_logging)
 
         if res:
             return res
         else:
-            raise RequestException("Setting %s with uid %s failed." % (db_object_type, uid))
+            raise RequestException(f"Setting {db_object_type} with uid {uid} failed.", context=context)
 
     @staticmethod
     def _user_agent():
         return f"encord-sdk-python/{encord_version} python/{platform.python_version()}"
 
+    @staticmethod
+    def _tracing_id() -> str:
+        return f"{uuid.uuid4().hex}/{random.randint(1, 2**63 - 1)};o=1"
+
+    @staticmethod
+    def _exception_context_from_response(response: Response) -> RequestContext:
+        try:
+            x_cloud_trace_context = response.headers.get(HEADER_CLOUD_TRACE_CONTEXT)
+            if x_cloud_trace_context is None:
+                return RequestContext()
+
+            x_cloud_trace_context = x_cloud_trace_context.split(";")[0]
+            trace_id, span_id = (x_cloud_trace_context.split("/") + [None, None])[:2]
+            return RequestContext(trace_id=trace_id, span_id=span_id)
+        except Exception:
+            return RequestContext()
+
     def _request(self, method, db_object_type: Type[T], uid, timeout, payload=None):
         request = Request(method, db_object_type, uid, timeout, self._config.connect_timeout, payload)
 
         request.headers = self._config.define_headers(request.data)
-        request.headers["User-Agent"] = self._user_agent()
+        request.headers[HEADER_USER_AGENT] = self._user_agent()
+        request.headers[HEADER_CLOUD_TRACE_CONTEXT] = self._tracing_id()
 
         return request
 
-    def _execute(self, request, enable_logging=True) -> Any:
+    def _execute(self, request: Request, enable_logging=True) -> Tuple[Any, RequestContext]:
         """Execute a request."""
         if enable_logging:
             logger.info("Request: %s", (request.data[:100] + "..") if len(request.data) > 100 else request.data)
@@ -156,18 +187,19 @@ class Querier:
             max_retries=req_settings.max_retries, backoff_factor=req_settings.backoff_factor
         ) as session:
             res = session.send(req, timeout=timeouts)
+            context = self._exception_context_from_response(res)
 
             try:
                 res_json = res.json()
             except Exception as e:
-                raise EncordException("Error parsing JSON response: %s" % res.text) from e
+                raise RequestException(f"Error parsing JSON response: {res.text}", context=context) from e
 
         if res_json.get("status") != requests.codes.ok:
             response = res_json.get("response")
             extra_payload = res_json.get("payload")
-            check_error_response(response, extra_payload)
+            check_error_response(response, context, extra_payload)
 
-        return res_json.get("response")
+        return res_json.get("response"), context
 
 
 @contextmanager
