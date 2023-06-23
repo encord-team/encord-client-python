81d925b87b5db9ae0dcf019f8c56805d2c70603a
Sergei Chertkov <sergei@encord.com>
2023-06-23 18:44:59 +0100

    downgrade minimal pydantic version

--- encord/orm/base_dto/__init__.py
+++ encord/orm/base_dto/__init__.py
@@ -1,6 +1,13 @@
-import pydantic
+try:
+    # For Python 3.8 and later
+    import importlib.metadata as importlib_metadata
+except ImportError:
+    # For everyone else
+    import importlib_metadata
 
-pydantic_version = int(pydantic.__version__.split(".")[0])
+pydantic_version = importlib_metadata.version("pydantic")
+
+pydantic_version = int(pydantic_version.split(".")[0])
 if pydantic_version < 2:
     from encord.orm.base_dto.base_dto_pydantic_v1 import BaseDTO, GenericBaseDTO
 else:
