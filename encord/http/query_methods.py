6f5644fcd021eb94b81b2455a396287e22b4cbb7
Sergei Chertkov <sergei@encord.com>
2023-06-26 14:29:37 +0100

    Rollback queryMethods enum changes

--- encord/http/query_methods.py
+++ encord/http/query_methods.py
@@ -12,10 +12,9 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-from encord.common.enum import StringEnum
 
 
-class QueryMethods(StringEnum):
+class QueryMethods(object):
     GET = "GET"
     POST = "POST"
     PUT = "PUT"
