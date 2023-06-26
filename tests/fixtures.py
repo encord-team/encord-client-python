6d6d051511822b867bd5ba1b73be494768e4f6f1
Sergei Chertkov <sergei@encord.com>
2023-06-26 13:51:26 +0100

    Add unit tests

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,8 +1,15 @@
+from unittest.mock import patch
+
 import pytest
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
 
 from encord import EncordUserClient
+from encord.client import EncordClientProject
+from encord.objects.ontology_labels_impl import Ontology as OrmOntology
+from encord.ontology import Ontology
+from encord.orm.project import Project as OrmProject
+from tests.test_data.ontology_blurb import ONTOLOGY_BLURB
 
 DUMMY_PRIVATE_KEY = (
     Ed25519PrivateKey.generate()
@@ -16,6 +23,23 @@ DUMMY_PRIVATE_KEY = (
 
 
 @pytest.fixture
+def ontology():
+    ontology = Ontology(None, None, OrmOntology.from_dict(ONTOLOGY_BLURB))
+    return ontology
+
+
+@pytest.fixture
 def user_client():
     client = EncordUserClient.create_with_ssh_private_key(DUMMY_PRIVATE_KEY)
     return client
+
+
+@pytest.fixture
+@patch.object(EncordClientProject, "get_project")
+def project(client_project_mock, user_client: EncordUserClient, ontology: Ontology):
+    client_project_mock.return_value = OrmProject(
+        {"ontology_hash": "dummy-ontology-hash", "project_hash": "dummy-project-hash"}
+    )
+    project = user_client.get_project("dummy-project-hash")
+    project._ontology = ontology
+    return project
