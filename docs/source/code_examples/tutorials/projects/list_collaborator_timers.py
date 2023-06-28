4d120560cb8ce9a47f9a74ca48fb33e6eafc311e
Sergei Chertkov <sergei@encord.com>
2023-06-28 12:14:58 +0100

    Update docs to match the method naming change

--- docs/source/code_examples/tutorials/projects/list_collaborator_timers.py
+++ docs/source/code_examples/tutorials/projects/list_collaborator_timers.py
@@ -1,3 +1,5 @@
+import datetime
+
 from encord import EncordUserClient, Project
 
 user_client: EncordUserClient = EncordUserClient.create_with_ssh_private_key(
@@ -5,7 +7,7 @@ user_client: EncordUserClient = EncordUserClient.create_with_ssh_private_key(
 )
 project: Project = user_client.get_project("<project_hash>")
 
-for timer in project.list_collaborator_sessions(
+for timer in project.list_collaborator_timers(
     after=datetime.datetime.now(datetime.timezone.utc)
     - datetime.timedelta(weeks=1)
 ):
