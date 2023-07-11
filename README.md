6efb32677f729e7b54f4d7e3bb64af1dadc551bd
Sergei Chertkov <sergei@encord.com>
2023-07-11 16:44:27 +0100

    Set minimal python to 3.8 (#382)
    * Set minimum python version to 3.8
    
    * Remove python 3.7 from github actions
    
    * Remove 3.7 from readme

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 ## 💻 Features
 
 - Minimal low-level Python client that allows you to interact with Encord's API
-- Supports Python: `3.7`, `3.8`, `3.9`, `3.10` and `3.11`
+- Supports Python: `3.8`, `3.9`, `3.10` and `3.11`
 
 ## ✨ Relevant Links
 
--- encord/_version.py
+++ encord/_version.py
@@ -1,8 +1,3 @@
-try:
-    # For Python 3.8 and later
-    import importlib.metadata as importlib_metadata
-except ImportError:
-    # For everyone else
-    import importlib_metadata  # type: ignore[no-redef]
+import importlib.metadata as importlib_metadata
 
 __version__ = importlib_metadata.version("encord")
