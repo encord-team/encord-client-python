7da7693e084cbaa573d0b798cee6251def2aaaef
Sergei Chertkov <sergei@encord.com>
2023-08-01 09:03:10 +0100

    Move all tests to separate folder to not to pollute the module itself

--- tests/orm/test_dataset.py
+++ tests/orm/test_dataset.py
@@ -121,3 +121,18 @@ def test_dataset_backwards_compatibility():
 def test_storage_location_default():
     assert StorageLocation(0) == StorageLocation.CORD_STORAGE
     assert StorageLocation(100) == StorageLocation.NEW_STORAGE
+
+
+def test_storage_location_to_str() -> None:
+    unique_representations: Set[StorageLocation] = set()
+
+    for location in StorageLocation:
+        string_representation = StorageLocation.get_str(location)
+        location_from_string = StorageLocation.from_str(string_representation)
+
+        unique_representations.add(location)
+
+        assert isinstance(string_representation, str)
+        assert location == location_from_string
+
+    assert len(unique_representations) == len(StorageLocation)
