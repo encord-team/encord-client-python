ceac8edc7b8e319dc434950125090b62d0c3113b
Sergei Chertkov <sergei@encord.com>
2024-01-04 17:06:13 +0000

    Minor cleanup

--- encord/client.py
+++ encord/client.py
@@ -409,10 +409,9 @@ class EncordClientDataset(EncordClient):
         """
 
         payload = {"user_emails": user_emails, "user_role": user_role}
-
         users = self._querier.basic_setter(DatasetUsers, self._config.resource_id, payload=payload)
 
-        return list(map(lambda user: DatasetUser.from_dict(user), users))
+        return [DatasetUser.from_dict(user) for user in users]
 
     def upload_video(
         self,
