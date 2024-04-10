1fe3e6dd08716d2e45afd6cf58224c47c81cb666
Sergei Chertkov <sergei@encord.com>
2024-04-10 17:49:49 +0100

    Fix linter warning

--- encord/orm/project.py
+++ encord/orm/project.py
@@ -163,9 +163,11 @@ class ProjectUsers:
 class ProjectDataset:
     pass
 
+
 class ProjectType(str, Enum):
     WORKFLOW = "workflow"
     MANUAL_QA = "manual_qa"
+
 
 class ProjectCopyOptions(str, Enum):
     COLLABORATORS = "collaborators"
