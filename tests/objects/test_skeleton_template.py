866b66bdea4273f788ec1774569295ed88f3b285
Jim Broadbent <jim@encord.com>
2024-04-16 11:06:41 +0100

    Additional test

--- tests/objects/test_skeleton_template.py
+++ tests/objects/test_skeleton_template.py
@@ -1,4 +1,5 @@
 import pytest
+from deepdiff import DeepDiff
 
 import encord.objects
 import encord.objects.skeleton_template
@@ -21,6 +22,60 @@ SKELETON_TEMPLATE_LINE = SkeletonTemplate.from_dict(
 )
 
 
-def test_skeleton_template_round_trip():
+def test_skeleton_template_round_trip_internal():
     dict_template = SKELETON_TEMPLATE_LINE.to_dict()
     assert SkeletonTemplate.from_dict(dict_template) == SKELETON_TEMPLATE_LINE
+
+SKELETON_TEMPLATE_TRIANGLE_JSON = {"name": "Triangle",
+        "shape": "skeleton",
+        "width": 0.235,
+        "height": 0.25,
+        "skeleton": {
+            "0": {
+                "x": 0.5148689289969273,
+                "y": 0.5,
+                "name": "point_0",
+                "color": "#000000",
+                "value": "point_0",
+                "featureHash": "A9sGmBcx"
+            },
+            "1": {
+                "x": 0.75,
+                "y": 0.5,
+                "name": "point_1",
+                "color": "#000000",
+                "value": "point_1",
+                "featureHash": "UWKgC/Dy"
+            },
+            "2": {
+                "x": 0.675,
+                "y": 0.25,
+                "name": "point_2",
+                "color": "#000000",
+                "value": "point_2",
+                "featureHash": "mBt9AAhC"
+            }
+        },
+        "skeletonEdges": {
+            "0": {
+                "1": {
+                    "color": "#000000"
+                }
+            },
+            "1": {
+                "2": {
+                    "color": "#000000"
+                }
+            },
+            "2": {
+                "0": {
+                    "color": "#000000"
+                }
+            }
+        },
+        "feature_node_hash": "GSc3nz5D"
+    }
+
+def test_skeleton_template_round_trip_external():
+    st = SkeletonTemplate.from_dict(SKELETON_TEMPLATE_TRIANGLE_JSON)
+    assert not DeepDiff(st.to_dict(), SKELETON_TEMPLATE_TRIANGLE_JSON, ignore_order=True)
