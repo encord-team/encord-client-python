07623498c537f920b2cf1245b7f28932ee669969
Jim Broadbent <jim@encord.com>
2024-04-16 11:08:50 +0100

    Use BaseDTO with custom alias

--- encord/orm/skeleton_template.py
+++ encord/orm/skeleton_template.py
@@ -1,5 +1,7 @@
 from typing import Any, Dict, List, Optional, Set, Union
 
+from pydantic import Field
+
 from encord.objects.coordinates import SkeletonCoordinate, SkeletonCoordinates
 from encord.orm.base_dto import BaseDTO
 
@@ -19,30 +21,6 @@ class SkeletonTemplate(BaseDTO):
     height: float
     skeleton: Dict[str, SkeletonTemplateCoordinate]
     skeleton_edges: Dict[str, Dict[str, Dict[str, str]]]  # start-end-color-hex
-    feature_node_hash: Optional[str] = None
+    feature_node_hash: Optional[str] = Field(default=None, alias="feature_node_hash")
     shape: Optional[str] = "skeleton"
 
-    @classmethod
-    def from_dict(cls, d: Dict[str, Any]):
-        coords = {key: SkeletonTemplateCoordinate.from_dict(val) for key, val in d["skeleton"].items()}
-        return cls(
-            name=d["name"],
-            width=d["width"],
-            height=d["height"],
-            skeleton=coords,
-            skeleton_edges=d["skeletonEdges"],
-            shape=d["shape"],
-            feature_node_hash=d["feature_node_hash"],
-        )
-
-    def to_dict(self, by_alias=True, exclude_none=True) -> Dict[str, Any]:
-        dict_skeleton = {key: value.to_dict() for key, value in self.skeleton.items()}
-        return {
-            "name": self.name,
-            "width": self.width,
-            "height": self.height,
-            "skeleton": dict_skeleton,
-            "skeletonEdges": self.skeleton_edges,
-            "feature_node_hash": self.feature_node_hash,
-            "shape": self.shape,
-        }
