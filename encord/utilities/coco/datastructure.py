19f2568a686ed4d1a86bc0a93f620b827bd814b3
Jim Broadbent <jim@encord.com>
2024-05-31 14:14:10 +0100

    Basic importer somewhat derived from Active

---
+++ encord/utilities/coco/datastructure.py
@@ -0,0 +1,129 @@
+from dataclasses import dataclass
+from datetime import date
+from typing import List, Optional, Set, Tuple, TypeAlias
+
+from encord.objects.common import Shape
+
+COCO_PERMANENT_ANNOTATIONS = [
+    "id_",
+    "image_id",
+    "category_id",
+    "segmentation",
+    "area",
+    "bbox",
+    "iscrowd",
+    "keypoints",
+    "num_keypoints",
+]
+ImageID = int
+CategoryID = int
+
+class SuperClass:
+    def asdict(self) -> dict:
+        pass
+
+@dataclass
+class FrameIndex:
+    label_hash: str
+    frame: int = 0
+
+@dataclass
+class CocoInfo:
+    # DENIS: extensibility??
+    contributor: Optional[str]
+    date_created: date
+    url: str
+    version: str  # DENIS: figure this out
+    year: str
+    description: str
+
+
+@dataclass
+class CocoCategory:
+    supercategory: str
+    id_: int
+    name: str
+
+
+@dataclass
+class CocoImage:
+    id_: int
+    height: int
+    width: int
+    file_name: str
+    coco_url: str
+    flickr_url: str
+
+
+CocoBbox = Tuple[float, float, float, float]
+"""x, y, w, h"""
+
+
+@dataclass
+class CocoAnnotation(SuperClass):
+    #  DENIS: does this depend on the format? Can this be extended?
+    area: float
+    bbox: CocoBbox
+    category_id: CategoryID
+    id_: int  # DENIS: how is this translated to the json. => maybe a wrapper around asdict
+    image_id: ImageID
+    iscrowd: int
+    segmentation: List  # DENIS: this is actually some union
+    keypoints: Optional[List[int]] = None
+    num_keypoints: Optional[int] = None
+    track_id: Optional[int] = None
+    encord_track_uuid: Optional[str] = None
+    rotation: Optional[float] = None
+
+
+@dataclass
+class CocoResult(SuperClass):
+    image_id: ImageID
+    category_id: int
+    score: float
+    bbox: Optional[CocoBbox] = None
+    segmentation: Optional[List] = None  # DENIS: this is actually some union]
+    keypoints: Optional[List[int]] = None
+
+
+@dataclass
+class Coco:
+    info: CocoInfo
+    licenses: List[dict]  # TODO:
+    categories: List[CocoCategory]
+    images: List[CocoImage]
+    annotations: List[CocoAnnotation]
+
+
+@dataclass
+class EncordCocoMetadata:
+    image_id: ImageID
+    image_url: str
+    image_path: str
+    image_title: str
+    label_hash: str
+    data_hash: str
+
+
+@dataclass
+class CocoCategoryInfo:
+    shapes: Set[Shape] = {}
+    has_rotation: bool = False
+
+
+
+def to_attributes_field(res: dict, include_null_annotations: bool = False) -> dict:
+    res_tmp = {}
+    for key, value in res.items():
+        if value is None and not include_null_annotations:
+            continue
+
+        if key in COCO_PERMANENT_ANNOTATIONS:
+            if key == "id_":
+                res_tmp["id"] = value
+            else:
+                res_tmp[key] = value
+        else:
+            attr = res_tmp.setdefault("attributes", {})
+            attr[key] = value
+    return res_tmp
---
+++ encord/utilities/coco/importer.py
@@ -0,0 +1,107 @@
+import json
+import logging
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Any, Dict, List, Optional, Set, Tuple
+
+from datastructure import CategoryID, CocoAnnotation, CocoCategoryInfo, CocoImage, FrameIndex, ImageID
+from parsers import (
+    parse_annotations,
+)
+from utils import crop_box_to_image_size
+
+from encord.objects.common import Shape
+from encord.objects.coordinates import BoundingBoxCoordinates, PointCoordinate, PolygonCoordinates
+from encord.objects.ontology_labels_impl import LabelRowV2
+from encord.objects.ontology_object import Object
+from encord.objects.ontology_structure import OntologyStructure
+from encord.project import Project
+from encord.utilities import label_utilities
+
+logger = logging.getLogger()
+
+class CocoImporter:
+    def __init__(self, project: Project, annotation_dict: Dict[str, Any], category_id_to_feature_hash: Dict[CategoryID,str], image_id_to_frame_index: Dict[ImageID,FrameIndex]) -> None:
+        self._project = project
+        self._annotation_dict = annotation_dict
+        self._category_id_to_feature_hash = category_id_to_feature_hash
+        self._image_id_to_frame_index = image_id_to_frame_index
+
+        self.populate_maps()
+
+        self._annotations = parse_annotations(annotation_dict.get("annotations", []))
+
+    def populate_maps(self) -> None:
+        map_category_to_encord_object: dict[CategoryID, Object] = {}
+        for id, feature_hash in self._category_id_to_feature_hash.items():
+            object = self._project.ontology_structure.get_child_by_hash(feature_node_hash=feature_hash)
+            map_category_to_encord_object[id] = object
+        self.category_to_encord = map_category_to_encord_object
+
+        # map_image_id_to_label_row: dict[ImageID, LabelRowV2] = {}
+        label_hashes = list({frame_index.label_hash for frame_index in self._image_id_to_frame_index.values()})
+        label_rows = self._project.list_label_rows_v2(label_hashes=label_hashes)
+        self._label_rows = label_rows
+
+    def encode(self) -> None:
+        for image_id, annotations in self._annotations.items():
+            frame_index = self._image_id_to_frame_index[image_id]
+            label_row = [row for row in self._label_rows if row.label_hash == frame_index.label_hash][0]
+            frame_view = label_row.get_frame_view(self._image_id_to_frame_index[image_id].frame)
+            img_h, img_w = frame_view.height, frame_view.width
+            for annotation in annotations:
+                encord_object = self.category_to_encord[annotation.category_id]
+                if annotation.segmentation:
+                    polygon = annotation.segmentation
+                    points = [PointCoordinate(x=polygon[i]/ img_w, y=polygon[i + 1] / img_h) for i in range(len(0,polygon, 2))]
+                    polygon_coordinates = PolygonCoordinates(values=points)
+                    object_instance = encord_object.create_instance()
+                    object_instance.set_for_frames(polygon_coordinates, frames=frame_index.frame)
+                    label_row.add_object_instance(object_instance)
+                elif len(annotation.bbox or []) == 4:
+                    try:
+                        x, y, w, h = crop_box_to_image_size(*annotation.bbox, img_w=img_w, img_h=img_h)
+                    except ValueError as e:
+                        logger.warning(f"<magenta>Skipping annotation with id {annotation.id_}</magenta> {str(e)}")
+                        continue
+                    bounding_box = BoundingBoxCoordinates(height = h /img_h, width=w /img_w, top_left_x=x /img_w, top_left_y=y/img_h)
+                    object_instance = encord_object.create_instance()
+                    object_instance.set_for_frames(bounding_box, frames=frame_index.frame)
+            label_row.save()
+
+
+
+
+
+
+# class CocoImporter:
+#     def __init__(
+#         self, images_dir_path: Path, annotations_file_path: Path, destination_dir: Path, use_symlinks: bool = False
+#     ):
+#         """
+#         Importer for COCO datasets.
+#         Args:
+#             images_dir_path (Path): Path where images are stored
+#             annotations_file_path (Path): The COCO JSON annotation file
+#             destination_dir (Path): Where to store the data
+#             use_symlinks (bool): If False, the importer will copy images.
+#                 Otherwise, symlinks will be used to save disk space.
+#         """
+#         self.images_dir = images_dir_path
+#         self.annotations_file_path = annotations_file_path
+#         self.use_symlinks: bool = use_symlinks
+#         self.data_hash_to_image_id: dict[str, int] = {}
+
+#         if not self.images_dir.is_dir():
+#             raise NotADirectoryError(f"Images directory '{self.images_dir}' doesn't exist")
+#         if not self.annotations_file_path.is_file():
+#             raise FileNotFoundError(f"Annotation file '{self.annotations_file_path}' doesn't exist")
+
+#         annotations_file = json.loads(self.annotations_file_path.read_text(encoding="utf-8"))
+#         self.info = parse_info(annotations_file["info"])
+#         self.categories = parse_categories(annotations_file["categories"])
+#         self.annotations = parse_annotations(annotations_file["annotations"])
+
+#         self.category_shapes = _infer_category_shapes(self.annotations)
+#         self.id_mappings: Dict[Tuple[int, Shape], int] = {}
+
