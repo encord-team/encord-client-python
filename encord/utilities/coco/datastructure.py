ce0b20b77ff9dc970fe6f31c3a47f3c0debb768d
Frederik Hvilshøj <frederik@cord.tech>
2024-06-07 11:11:45 +0200

    an alternative to coco import with pydantic parsing

--- encord/utilities/coco/datastructure.py
+++ encord/utilities/coco/datastructure.py
@@ -1,7 +1,30 @@
-from dataclasses import dataclass, field
-from typing import List, NamedTuple, Optional, Union
+import importlib.metadata as importlib_metadata
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Any, NamedTuple
 
-from nptyping import NDArray
+from pydantic import BaseModel, RootModel, model_validator
+
+from encord.objects.bitmask import BitmaskCoordinates
+from encord.objects.coordinates import (
+    BoundingBoxCoordinates,
+    PointCoordinate,
+    PolygonCoordinates,
+)
+
+try:
+    import pycocotools.mask as coco_mask_utils
+except ModuleNotFoundError:
+    raise ModuleNotFoundError(
+        "The optional dependency `pycocotools` must be installed to import coco projects. You can install it with `pip install encord[pycocotools]"
+    )
+
+
+pydantic_version_str = importlib_metadata.version("pydantic")
+pydantic_version = int(pydantic_version_str.split(".")[0])
+if pydantic_version < 2:
+    ModuleNotFoundError("To be able to import coco projects, you must have `pydantic>=2.0`")
+
 
 ImageID = int
 CategoryID = int
@@ -13,24 +36,119 @@ class FrameIndex:
     frame: int = 0
 
 
-class CocoBbox(NamedTuple):
+class CocoInfoModel(BaseModel):
+    year: int | None
+    version: str | None
+    description: str | None
+    contributor: str | None
+    url: str | None
+    date_created: str | None
+
+
+class CocoLicenseModel(BaseModel):
+    id: int
+    name: str
+    url: str
+
+
+class CocoImageModel(BaseModel):
+    id: ImageID
+    width: int
+    height: int
+    file_name: str | None
+    license: int | None
+    flickr_url: str | None
+    coco_url: str | None
+    date_captured: datetime
+
+
+class CocoCategoryModel(BaseModel):
+    id: CategoryID
+    name: str
+    supercategory: str | None
+
+
+# ints in documentation but this gives more flexibility
+class CocoBoundingBox(NamedTuple):
     x: float
     y: float
     w: float
     h: float
 
+    def to_encord(self, img_w: int, img_h: int) -> BoundingBoxCoordinates:
+        return BoundingBoxCoordinates(
+            top_left_x=self.x / img_w,
+            top_left_y=self.y / img_h,
+            width=self.w / img_w,
+            height=self.h / img_h,
+        )
 
-@dataclass
-class CocoAnnotation:
-    area: float
-    bbox: CocoBbox
-    category_id: CategoryID
-    id_: int
+
+class RLESize(NamedTuple):
+    height: int
+    width: int
+
+
+class PointTuple(NamedTuple):
+    x: float
+    y: float
+
+
+class CocoPolygon(RootModel):
+    root: list[PointTuple]
+
+    @model_validator(mode="before")
+    @classmethod
+    def validate_polyton(cls, _value: Any) -> Any:
+        if not (isinstance(_value, list) and len(_value) > 0):
+            return _value
+        if isinstance(_value[0], list):
+            return
+
+        return [[_value[i], _value[i + 1]] for i in range(0, len(_value), 2)]
+
+    def to_encord(self, img_w: int, img_h: int) -> PolygonCoordinates:
+        return PolygonCoordinates(values=[PointCoordinate(x=p.x / img_w, y=p.y / img_h) for p in self.root])
+
+
+class CocoRLE(BaseModel):
+    size: RLESize
+    counts: bytes
+
+    @model_validator(mode="before")
+    @classmethod
+    def parse_rle(cls, _value: Any) -> Any:
+        if not isinstance(_value, dict):
+            # This is not RLE
+            return None
+
+        h, w = _value["size"]
+        if isinstance(_value, list):
+            rles = coco_mask_utils.frPyObjects(_value, h, w)
+            rle = coco_mask_utils.merge(rles)
+        elif isinstance(_value["counts"], list):
+            rle = coco_mask_utils.frPyObjects(_value, h, w)
+        else:
+            rle = _value
+        return rle
+
+    def to_encord(self) -> BitmaskCoordinates:
+        return BitmaskCoordinates(coco_mask_utils.decode(self.dict()).astype(bool))
+
+
+class CocoAnnotationModel(BaseModel):
+    id: int
     image_id: ImageID
-    iscrowd: int
-    segmentation: Union[List, NDArray]
-    keypoints: Optional[List[int]] = None
-    num_keypoints: Optional[int] = None
-    track_id: Optional[int] = None
-    encord_track_uuid: Optional[str] = None
-    rotation: Optional[float] = None
+    category_id: int
+    segmentation: CocoPolygon | list[CocoPolygon] | CocoRLE
+    area: float
+    bbox: CocoBoundingBox
+    iscrowd: bool
+
+
+class CocoRootModel(BaseModel):
+    info: CocoInfoModel
+    categories: list[CocoCategoryModel]
+    images: list[CocoImageModel]
+    annotations: list[CocoAnnotationModel]
+    licenses: list[CocoLicenseModel]
--- encord/utilities/coco/importer.py
+++ encord/utilities/coco/importer.py
@@ -1,113 +1,139 @@
 import logging
 from dataclasses import dataclass, field
-from typing import TYPE_CHECKING, Any, Dict, List, Optional, Set, Tuple, Union, ValuesView
-
-from nptyping import NDArray
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Dict,
+    List,
+    Optional,
+    Set,
+    Tuple,
+    Union,
+    ValuesView,
+)
 
 from encord.objects.bitmask import BitmaskCoordinates
 from encord.objects.common import Shape
-from encord.objects.coordinates import BoundingBoxCoordinates, PointCoordinate, PolygonCoordinates
+from encord.objects.coordinates import (
+    BoundingBoxCoordinates,
+    PointCoordinate,
+    PolygonCoordinates,
+)
+from encord.objects.ontology_labels_impl import LabelRowV2
 from encord.objects.ontology_object import Object
+from encord.project import Project
 from encord.utilities.coco.datastructure import (
     CategoryID,
+    CocoPolygon,
+    CocoRLE,
+    CocoRootModel,
     FrameIndex,
     ImageID,
 )
-from encord.utilities.coco.parsers import (
-    parse_annotations,
-)
-from encord.utilities.coco.utils import crop_box_to_image_size
-
-if TYPE_CHECKING:
-    from encord import Project
 
 logger = logging.getLogger()
 
 
-class CocoImporter:
-    def __init__(
-        self,
-        project: "Project",
-        annotation_dict: Dict[str, Any],
-        category_id_to_feature_hash: Dict[CategoryID, str],
-        image_id_to_frame_index: Dict[ImageID, FrameIndex],
-    ) -> None:
-        self._project = project
-        self._annotation_dict = annotation_dict
-        self._category_id_to_feature_hash = category_id_to_feature_hash
-        self._image_id_to_frame_index = image_id_to_frame_index
+def build_category_id_to_encord_ontology_object_map(
+    project: Project,
+    category_id_to_feature_hash: Dict[CategoryID, str],
+) -> dict[CategoryID, Object]:
+    map_category_to_encord_object: dict[CategoryID, Object] = {}
+    for id, feature_hash in category_id_to_feature_hash.items():
+        object_ = project.ontology_structure.get_child_by_hash(feature_node_hash=feature_hash, type_=Object)
+        map_category_to_encord_object[id] = object_
 
-        self.populate_maps()
+    return map_category_to_encord_object
 
-        self._annotations = parse_annotations(annotation_dict.get("annotations", []))
 
-    def populate_maps(self) -> None:
-        map_category_to_encord_object: Dict[CategoryID, Object] = {}
-        for id, feature_hash in self._category_id_to_feature_hash.items():
-            object: Object = self._project.ontology_structure.get_child_by_hash(feature_node_hash=feature_hash)
-            map_category_to_encord_object[id] = object
-        self.category_to_encord = map_category_to_encord_object
+def initialise_label_rows(
+    project: Project, image_id_to_frame_index: dict[ImageID, FrameIndex]
+) -> dict[str, LabelRowV2]:
+    data_hashes = list({frame_index.data_hash for frame_index in image_id_to_frame_index.values()})
+    label_rows = project.list_label_rows_v2(data_hashes=data_hashes)
+    with project.create_bundle() as bundle:
+        for lr in label_rows:
+            lr.initialise_labels(bundle=bundle)
+    return {lr.data_hash: lr for lr in label_rows}
 
-        data_hashes = list({frame_index.data_hash for frame_index in self._image_id_to_frame_index.values()})
-        label_rows = self._project.list_label_rows_v2(data_hashes=data_hashes)
-        with self._project.create_bundle() as bundle:
-            for row in label_rows:
-                row.initialise_labels(bundle=bundle)
-        self._label_rows = label_rows
 
-    def encode(self) -> None:
-        for image_id, annotations in self._annotations.items():
-            frame_index = self._image_id_to_frame_index[image_id]
-            rows = [row for row in self._label_rows if row.data_hash == frame_index.data_hash]
-            if not len(rows) == 1:
-                raise ValueError(f"Data hash {frame_index.data_hash} not found in project")
-            label_row = rows[0]
-            frame_view = label_row.get_frame_view(self._image_id_to_frame_index[image_id].frame)
-            img_h, img_w = frame_view.height, frame_view.width
-            for annotation in annotations:
-                encord_object = self.category_to_encord[annotation.category_id]
-                if annotation.segmentation is not None and encord_object.shape in [Shape.POLYGON, Shape.BITMASK]:
-                    coordinates: Union[PolygonCoordinates, BitmaskCoordinates]
-                    if encord_object.shape == Shape.POLYGON:
-                        polygon = annotation.segmentation
-                        points = [
-                            PointCoordinate(x=polygon[i] / img_w, y=polygon[i + 1] / img_h)
-                            for i in range(0, len(polygon), 2)
-                        ]
-                        coordinates = PolygonCoordinates(values=points)
-                    elif encord_object.shape == Shape.BITMASK:
-                        mask = annotation.segmentation
-                        if not isinstance(mask, NDArray):
-                            raise ValueError("Require Bitmask for mask")
-                        coordinates = BitmaskCoordinates(mask.astype(bool))
-                    else:
-                        # Impossible
-                        pass
-                    object_instance = encord_object.create_instance()
-                    object_instance.set_for_frames(coordinates, frames=frame_index.frame)
-                    label_row.add_object_instance(object_instance)
-                elif encord_object.shape == Shape.BOUNDING_BOX:
-                    try:
-                        x, y, w, h = crop_box_to_image_size(
-                            annotation.bbox.x,
-                            annotation.bbox.y,
-                            annotation.bbox.w,
-                            annotation.bbox.h,
-                            img_w=img_w,
-                            img_h=img_h,
-                        )
-                    except ValueError as e:
-                        logger.warning(f"Skipping annotation with id {annotation.id_} {str(e)}")
-                        continue
-                    bounding_box = BoundingBoxCoordinates(
-                        height=h / img_h, width=w / img_w, top_left_x=x / img_w, top_left_y=y / img_h
-                    )
-                    object_instance = encord_object.create_instance()
-                    object_instance.set_for_frames(bounding_box, frames=frame_index.frame)
-                    label_row.add_object_instance(object_instance)
-                else:
-                    logger.warning(
-                        "Should have either Bitmask/Polygon with segmentation data or be matching to Bounding box type."
-                    )
-                    continue
-            label_row.save()
+def import_coco_labels(
+    project: Project,
+    coco: CocoRootModel,
+    category_id_to_feature_hash: dict[CategoryID, str],
+    image_id_to_frame_index: dict[ImageID, FrameIndex],
+) -> None:
+    label_rows = initialise_label_rows(project, image_id_to_frame_index)
+    category_id_to_objects = build_category_id_to_encord_ontology_object_map(project, category_id_to_feature_hash)
+    coco_image_lookup = {i.id: i for i in coco.images}
+
+    for i, annotation in enumerate(coco.annotations):
+        frame_idx = image_id_to_frame_index.get(annotation.image_id)
+        if frame_idx is None:
+            # TODO not clear how to propagate errors
+            logger.warning(
+                f'Image id `{annotation.image_id}` form `labels_dict["annotations"][{i}]` could not be matched with the provided `image_id_to_frame_index`. Skipping this annotation.'
+            )
+            continue
+
+        label_row = label_rows.get(frame_idx.data_hash)
+        if label_row is None:
+            logger.warning(
+                f"Data hash `{frame_idx.data_hash}` form `image_id_to_frame_index` could not be matched with a data hash in the provided `project`. Skipping annotation ad index {i}."
+            )
+            # TODO not clear how to propagate errors
+            continue
+
+        ont_obj = category_id_to_objects.get(annotation.category_id)
+        if ont_obj is None:
+            logger.warning(
+                f'Category ID {annotation.category_id} from `labels_dict["annotations"][{i}]` could not be matched with the provided `category_id_to_feature_hash`. Skipping this annotation.'
+            )
+            # TODO not clear how to propagate errors
+            continue
+
+        coco_image = coco_image_lookup.get(annotation.image_id)
+        if coco_image is None:
+            raise ValueError(
+                f"The provided coco annotation dictionary have annotations with `image_id`s that do not match any image ids in the provided `images` list. Couldn't find image id {annotation.image_id}."
+            )
+
+        segmentation = annotation.segmentation
+        width, height = coco_image.width, coco_image.height
+        ins = ont_obj.create_instance()
+        if ont_obj.shape == Shape.BOUNDING_BOX:
+            ins.set_for_frames(
+                coordinates=annotation.bbox.to_encord(img_w=width, img_h=height),
+                frames=frame_idx.frame,
+            )
+            label_row.add_object_instance(ins)
+        elif ont_obj.shape == Shape.BITMASK:
+            if not isinstance(segmentation, CocoRLE):
+                raise ValueError(
+                    f"A mismatch between the format in the `labels_dict` and the expected shape in the ontology object with feature node hash {ont_obj.feature_node_hash} was detected for annotation id {annotation.id}. Expected format was an RLE."
+                )
+            ins.set_for_frames(coordinates=segmentation.to_encord(), frames=frame_idx.frame)
+            label_row.add_object_instance(ins)
+        elif ont_obj.shape == Shape.POLYGON:
+            if not isinstance(segmentation, CocoPolygon) or (
+                isinstance(segmentation, list) and isinstance(segmentation[0], CocoPolygon)
+            ):
+                raise ValueError(
+                    f"A mismatch between the format in the `labels_dict` and the expected shape in the ontology object with feature node hash {ont_obj.feature_node_hash} was detected for annotation id {annotation.id}. Expected format was a polygon or a list of polygons."
+                )
+            if isinstance(segmentation, CocoPolygon):
+                segmentation = [segmentation]
+
+            for poly in segmentation:
+                ins.set_for_frames(
+                    coordinates=poly.to_encord(img_w=width, img_h=height),
+                    frames=frame_idx.frame,
+                )
+                label_row.add_object_instance(ins)
+                ins = ont_obj.create_instance()
+        else:
+            raise ValueError(f"Ontology objects of shape {ont_obj.shape} are not supported for coco import")
+
+    with project.create_bundle() as bundle:
+        for label_row in label_rows.values():
+            label_row.save(bundle=bundle)
