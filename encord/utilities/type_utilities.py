b89bd0519b523ee27750c9ab4539105e8135cd13
Clinton Wee <clinton@encord.com>
2024-07-26 14:18:22 +0100

    Used correct import from typing

--- encord/utilities/type_utilities.py
+++ encord/utilities/type_utilities.py
@@ -1,7 +1,7 @@
-from typing import Never
+from typing import NoReturn, Optional
 
 
-def exhaustive_guard(value: Never, message: str | None = None) -> Never:
+def exhaustive_guard(value: NoReturn, message: Optional[str]) -> NoReturn:
     # This also works in runtime as well:
     error_message = message if message is not None else f"This code should never be reached, got: [{value}]"
     raise TypeError(error_message)
