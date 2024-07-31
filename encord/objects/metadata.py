681c902010cc3e0c75578e89f7ead7677288b7ef
Laverne-Encord <145356218+Laverne-Encord@users.noreply.github.com>
2024-07-31 16:18:13 +0100

    Updates doc strings for Metadata.py
    Updates doc strings for Metadata.py

--- encord/objects/metadata.py
+++ encord/objects/metadata.py
@@ -17,7 +17,12 @@ from encord.orm.base_dto import BaseDTO
 
 class DICOMSeriesMetadata(BaseDTO):
     """
-    Metadata for the DICOM series
+    Metadata for a DICOM series.
+
+    Attributes:
+        patient_id (Optional[str]): The ID of the patient. This attribute is optional.
+        study_uid (str): The unique identifier for the study.
+        series_uid (str): The unique identifier for the series.
     """
 
     patient_id: Optional[str]
@@ -27,7 +32,14 @@ class DICOMSeriesMetadata(BaseDTO):
 
 class DICOMSliceMetadata(BaseDTO):
     """
-    Metadata for a slice in a DICOM series
+    Metadata for a slice in a DICOM series.
+
+    Attributes:
+        dicom_instance_uid (str): The unique identifier for the DICOM instance.
+        multiframe_frame_number (Optional[int]): The frame number if the DICOM instance is a multiframe image. This attribute is optional.
+        file_uri (str): The URI to the file containing the slice.
+        width (int): The width of the slice in pixels.
+        height (int): The height of the slice in pixels.
     """
 
     dicom_instance_uid: str
