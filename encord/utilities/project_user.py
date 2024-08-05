b04ae3de53010e8a5d8278b122c79f071fbe41d5
Laverne-Encord <145356218+Laverne-Encord@users.noreply.github.com>
2024-08-05 09:38:04 +0100

    Updates doc strings for Project_User.py
    Updates doc strings for Project_User.py

--- encord/utilities/project_user.py
+++ encord/utilities/project_user.py
@@ -4,6 +4,17 @@ from encord.orm.base_dto import BaseDTO
 
 
 class ProjectUserRole(IntEnum):
+    """
+    Enumeration for user roles within a project.
+
+    Attributes:
+        ADMIN (int): Represents an admin user with value 0.
+        ANNOTATOR (int): Represents an annotator user with value 1.
+        REVIEWER (int): Represents a reviewer user with value 2.
+        ANNOTATOR_REVIEWER (int): Represents a user who is both an annotator and a reviewer with value 3.
+        TEAM_MANAGER (int): Represents a team manager user with value 4.
+    """
+
     ADMIN = (0,)
     ANNOTATOR = (1,)
     REVIEWER = (2,)
@@ -12,6 +23,15 @@ class ProjectUserRole(IntEnum):
 
 
 class ProjectUser(BaseDTO):
+    """
+    Data transfer object representing a user within a project.
+
+    Attributes:
+        user_email (str): The email address of the user.
+        user_role (ProjectUserRole): The role of the user in the project, defined by the ProjectUserRole enumeration.
+        project_hash (str): A unique identifier for the project.
+    """
+
     user_email: str
     user_role: ProjectUserRole
     project_hash: str
