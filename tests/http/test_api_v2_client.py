f0ec897067fb10041899fe5002494f947abe7b29
james-encord <james@encord.com>
2024-09-03 15:06:37 +0100

    fix: typo + more test coverage

--- tests/http/test_api_v2_client.py
+++ tests/http/test_api_v2_client.py
@@ -1,13 +1,13 @@
 import json
 from datetime import datetime
-from typing import Optional
 from unittest.mock import MagicMock, patch
 
 import pytest
 from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
-from requests import Response, Session
+from pydantic import BaseModel
+from requests import Session
 
-from encord.configs import SshConfig, UserConfig
+from encord.configs import SshConfig
 from encord.http.v2.api_client import ApiClient
 from encord.orm.base_dto import BaseDTO
 
@@ -18,7 +18,17 @@ class TestPayload(BaseDTO):
     payload: str
 
 
+class TestPayloadV2(BaseModel):
+    payload: str
+
+
 class TestComplexPayload(BaseDTO):
+    text: str
+    number: float
+    time: datetime
+
+
+class TestComplexPayloadV2(BaseModel):
     text: str
     number: float
     time: datetime
@@ -29,18 +39,19 @@ def api_client():
     return ApiClient(config=SshConfig(PRIVATE_KEY))
 
 
+@pytest.mark.parametrize("payload_type", [TestPayload, TestPayloadV2])
 @patch.object(Session, "send")
-def test_constructed_url_is_correct(send: MagicMock, api_client: ApiClient):
+def test_constructed_url_is_correct(send: MagicMock, api_client: ApiClient, payload_type: type[TestPayload]) -> None:
     mock_response = MagicMock()
     mock_response.status_code = 200
     mock_response.json.return_value = {"payload": "hello world"}
     mock_response.content = json.dumps({"payload": "hello world"})
     send.return_value = mock_response
 
-    api_client.get("/", params=None, result_type=TestPayload)
-    api_client.get("/test", params=None, result_type=TestPayload)
-    api_client.get("/test/", params=None, result_type=TestPayload)
-    api_client.get("test/", params=None, result_type=TestPayload)
+    api_client.get("/", params=None, result_type=payload_type)
+    api_client.get("/test", params=None, result_type=payload_type)
+    api_client.get("/test/", params=None, result_type=payload_type)
+    api_client.get("test/", params=None, result_type=payload_type)
 
     assert send.call_args_list[0].args[0].url == "https://api.encord.com/v2/public"
     assert send.call_args_list[1].args[0].url == "https://api.encord.com/v2/public/test"
@@ -48,8 +59,9 @@ def test_constructed_url_is_correct(send: MagicMock, api_client: ApiClient):
     assert send.call_args_list[3].args[0].url == "https://api.encord.com/v2/public/test"
 
 
+@pytest.mark.parametrize("payload_type", [TestPayload, TestPayloadV2])
 @patch.object(Session, "send")
-def test_payload_url_serialisation(send: MagicMock, api_client: ApiClient):
+def test_payload_url_serialisation(send: MagicMock, api_client: ApiClient, payload_type: type[TestPayload]) -> None:
     mock_response = MagicMock()
     mock_response.status_code = 200
     mock_response.json.return_value = {"payload": "hello world"}
@@ -59,7 +71,7 @@ def test_payload_url_serialisation(send: MagicMock, api_client: ApiClient):
     expected_time = datetime.fromisoformat("2024-02-01T01:02:03+01:00")
     payload = TestComplexPayload(text="test", number=0.01, time=expected_time)
 
-    api_client.get("/", params=payload, result_type=TestPayload)
+    api_client.get("/", params=payload, result_type=payload_type)
 
     assert (
         send.call_args_list[0].args[0].url
@@ -67,8 +79,12 @@ def test_payload_url_serialisation(send: MagicMock, api_client: ApiClient):
     )
 
 
+@pytest.mark.parametrize("payload_type", [TestPayload, TestPayloadV2])
+@pytest.mark.parametrize("param_type", [TestComplexPayload, TestComplexPayloadV2])
 @patch.object(Session, "send")
-def test_payload_body_serialisation(send: MagicMock, api_client: ApiClient):
+def test_payload_body_serialisation(
+    send: MagicMock, api_client: ApiClient, payload_type: type[TestPayload], param_type: type[TestComplexPayload]
+) -> None:
     mock_response = MagicMock()
     mock_response.status_code = 200
     mock_response.json.return_value = {"payload": "hello world"}
@@ -76,9 +92,9 @@ def test_payload_body_serialisation(send: MagicMock, api_client: ApiClient):
     send.return_value = mock_response
 
     expected_time = datetime.fromisoformat("2024-02-01T01:02:03+01:00")
-    payload = TestComplexPayload(text="test", number=0.01, time=expected_time)
+    payload = param_type(text="test", number=0.01, time=expected_time)
 
-    api_client.post("/", params=None, payload=payload, result_type=TestPayload)
+    api_client.post("/", params=None, payload=payload, result_type=payload_type)
 
     assert send.call_args_list[0].args[0].url == "https://api.encord.com/v2/public"
     assert (
