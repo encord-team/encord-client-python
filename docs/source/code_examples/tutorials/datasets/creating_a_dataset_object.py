8a1f79371b9a1d1a661c0d4d604f2b5aa5734b22
Adam Jochna <adam@encord.com>
2024-10-03 17:00:08 +0200

    Remove docs

--- docs/source/code_examples/tutorials/datasets/creating_a_dataset_object.py
+++
@@ -1,7 +0,0 @@
-from encord import Dataset, EncordUserClient
-
-user_client: EncordUserClient = EncordUserClient.create_with_ssh_private_key(
-    "<your_private_key>"
-)
-
-dataset: Dataset = user_client.get_dataset("<dataset_hash>")
--- docs/source/code_examples/tutorials/workflows/workflow_reopen-example.py
+++
@@ -1,9 +0,0 @@
-from encord import EncordUserClient, Project
-
-user_client: EncordUserClient = EncordUserClient.create_with_ssh_private_key(
-    "<your_private_key>"
-)
-project: Project = user_client.get_project("<workflows_project_hash>")
-
-for label_row in project.list_label_rows_v2():
-    label.workflow_reopen()
