1b4a0ef2819499511e6ccc6b7a432ea912cbedb9
Clinton Wee <clinton@encord.com>
2024-11-13 13:45:42 +0000

    Fix linting

--- encord/objects/ontology_labels_impl.py
+++ encord/objects/ontology_labels_impl.py
@@ -785,9 +785,7 @@ class LabelRowV2:
         frames = set(_frame_views_to_frame_numbers(object_instance.get_annotations()))
         self._add_to_frame_to_hashes_map(object_instance, frames)
 
-    def add_classification_instance(
-        self, classification_instance: ClassificationInstance, force: bool = False
-    ) -> None:
+    def add_classification_instance(self, classification_instance: ClassificationInstance, force: bool = False) -> None:
         """
         Add a classification instance to the label row.
 
