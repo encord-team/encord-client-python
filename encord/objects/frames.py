19fd563946ada934a1adcbe8092f919da5049096
clinton-encord <163850890+clinton-encord@users.noreply.github.com>
2024-11-13 11:54:47 +0000

    ED-295/export audio labels (#758)
    * Bump version number
    
    * Added range utilities
    
    * Added tests for range utilities
    
    * Fixed some tests
    
    * Added tests for audio sdk additions
    
    * Added some types to range_manager
    
    * Remove unnecessary todo
    
    * Improve performance by only using ranges for audio
    
    * ADded some comments
    
    * Added tests
    
    * Minor refactors and renaming
    
    * Added remove_from_ranges
    
    * Added annotation test
    
    * Implement git comments
    
    * Add comment
    
    * Ruff format
    
    * Updated range_list error message
    
    * Update doc string

--- encord/objects/frames.py
+++ encord/objects/frames.py
@@ -32,6 +32,18 @@ class Range:
     def __repr__(self):
         return f"({self.start}:{self.end})"
 
+    def overlaps(self, other) -> bool:
+        """Check if two ranges overlap"""
+        return self.start <= other.end and other.start <= self.end
+
+    def merge(self, other) -> None:
+        """Merge another range into this one."""
+        self.start = min(self.start, other.start)
+        self.end = max(self.end, other.end)
+
+    def copy(self) -> Range:
+        return Range(self.start, self.end)
+
 
 Ranges = List[Range]
 FramesList = List[int]
