5266c258d74641b392451aa8f680b01b47d3e1f7
Adam Encord <110395012+adam-encord@users.noreply.github.com>
2024-11-18 16:34:30 +0100

    SDK cloud integrations v2 API (#787)
    * SDK cloud integrations v2
    
    * Update typing

--- encord/orm/cloud_integration.py
+++ encord/orm/cloud_integration.py
@@ -1,6 +1,18 @@
+from typing import List
+from uuid import UUID
+
 from encord.orm.base_dto import BaseDTO
 
 
 class CloudIntegration(BaseDTO):
     id: str
     title: str
+
+
+class CloudIntegrationV2(BaseDTO):
+    integration_uuid: UUID
+    title: str
+
+
+class GetCloudIntegrationsResponse(BaseDTO):
+    result: List[CloudIntegrationV2]
