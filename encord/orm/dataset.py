64ccb4924dd7327642119f189850af4030ca777d
Adam Jochna <adam@encord.com>
2024-11-19 22:31:01 +0100

    Datasets API v2 - SDK

--- encord/orm/dataset.py
+++ encord/orm/dataset.py
@@ -1051,3 +1051,20 @@ class DatasetDataLongPolling(BaseDTO):
 @dataclasses.dataclass(frozen=True)
 class DatasetLinkItems:
     pass
+
+
+class CreateDatasetPayload(BaseDTO):
+    title: str
+    description: Optional[str]
+
+    create_backing_folder: bool  # this field will be removed soon
+
+    # only for analytics, to know if customers are
+    # using depreciated EncordUserClient.create_private_dataset
+    # this is only place which should pass legacy_call=True
+    legacy_call: bool  # this field will be removed soon
+
+
+class CreateDatasetResponseV2(BaseDTO):
+    dataset_hash: UUID
+    backing_folder_uuid: Optional[UUID] = None  # this field will be removed soon
