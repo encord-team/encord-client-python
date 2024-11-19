d69c39fc1c974d61c2cb4ab320fe944060e4edc6
Abhishek Deora <abhishek@encord.com>
2024-11-19 16:50:44 +0000

    fix: lint

--- encord/project.py
+++ encord/project.py
@@ -1165,7 +1165,7 @@ class Project:
 
     def list_collections(
         self,
-        collection_uuids: List[Union[str, UUID]] | None = None,
+        collection_uuids: Optional[List[Union[str, UUID]]] = None,
         page_size: Optional[int] = None,
     ) -> Iterator[ProjectCollection]:
         """
