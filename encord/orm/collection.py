602b1123de87eb6551c6a9a38cacf97fe308d2e9
deoracord <145985523+deoracord@users.noreply.github.com>
2024-11-25 16:00:02 +0000

    Feat: Active collections functions (#775)
    * feat: active collection operations and list
    
    * feat: active collection operations and list
    
    * feat: extended collection functions
    
    * feat: extended collection functions
    
    * feat: get one collection
    
    * feat: public api v2 return types
    
    * feat: public api v2 return types
    
    * fix: lint
    
    * fix: lint
    
    * fix: lint
    
    * fix: lint
    
    * fix: lint
    
    * fix: lint
    
    * fix: lint
    
    * fix: lint
    
    * fix: lint
    
    * fix: verbose type conv ersion
    
    * fix: verbose type conv ersion
    
    * fix: lint
    
    * fix: rebase
    
    * fix: rebase
    
    * fix: lint
    
    * fix: lint
    
    * fix: fps
    
    * fix: fps
    
    * fix: typos

--- encord/orm/collection.py
+++ encord/orm/collection.py
@@ -1,9 +1,11 @@
 import uuid
 from datetime import datetime
-from typing import List, Optional
+from enum import Enum
+from typing import Any, List, Optional, Union
 from uuid import UUID
 
 from encord.orm.base_dto import BaseDTO, Field
+from encord.orm.label_row import LabelRowMetadataDTO
 
 
 class GetCollectionParams(BaseDTO):
@@ -55,3 +57,75 @@ class CollectionBulkItemResponse(BaseDTO):
 
 class CollectionBulkPresetRequest(BaseDTO):
     preset_uuid: uuid.UUID
+
+
+class ProjectCollectionType(Enum):
+    FRAME = "FRAME"
+    LABEL = "LABEL"
+
+
+class GetProjectCollectionParams(BaseDTO):
+    project_hash: Optional[uuid.UUID] = Field(default=None, alias="projectHash")
+    collection_uuids: Optional[List[uuid.UUID]] = Field(default=[], alias="uuids")
+    page_token: Optional[str] = Field(default=None, alias="pageToken")
+    page_size: Optional[int] = Field(default=None, alias="pageSize")
+
+
+class CreateProjectCollectionParams(BaseDTO):
+    project_hash: Optional[uuid.UUID] = Field(default=None, alias="projectHash")
+
+
+class CreateProjectCollectionPayload(CreateCollectionPayload):
+    collection_type: ProjectCollectionType
+
+
+class ProjectCollection(BaseDTO):
+    collection_uuid: UUID
+    name: str
+    description: Optional[str]
+    created_at: Optional[datetime] = Field(default=None, alias="createdAt")
+    last_edited_at: Optional[datetime] = Field(default=None, alias="lastEditedAt")
+    project_uuid: UUID
+    collection_type: ProjectCollectionType
+
+
+class GetProjectCollectionsResponse(BaseDTO):
+    results: List[ProjectCollection]
+
+
+class ProjectDataCollectionInstance(BaseDTO):
+    frame: int
+
+
+class ProjectLabelCollectionInstance(BaseDTO):
+    frame: int
+    annotation_id: str
+
+
+class ProjectDataCollectionItemResponse(BaseDTO):
+    label_row_metadata: LabelRowMetadataDTO
+    instances: List[ProjectDataCollectionInstance]
+
+
+class ProjectLabelCollectionItemResponse(BaseDTO):
+    label_row_metadata: LabelRowMetadataDTO
+    instances: List[ProjectLabelCollectionInstance]
+
+
+class ProjectDataCollectionItemRequest(BaseDTO):
+    data_uuid: Union[str, UUID]
+    frame: int
+
+
+class ProjectLabelCollectionItemRequest(BaseDTO):
+    data_uuid: Union[str, UUID]
+    frame: int
+    annotation_id: str
+
+
+class ProjectCollectionBulkItemRequest(BaseDTO):
+    items: List[Union[ProjectDataCollectionItemRequest, ProjectLabelCollectionItemRequest]]
+
+
+class ProjectCollectionBulkItemResponse(BaseDTO):
+    failed_items: List[Union[ProjectDataCollectionItemRequest, ProjectLabelCollectionItemRequest]]
--- encord/orm/label_row.py
+++ encord/orm/label_row.py
@@ -6,6 +6,8 @@ from dataclasses import asdict, dataclass
 from enum import Enum
 from typing import Any, Dict, List, Optional
 
+from pydantic import Field
+
 from encord.common.time_parser import parse_datetime
 from encord.orm import base_orm
 from encord.orm.base_dto import BaseDTO
@@ -267,7 +269,7 @@ class LabelRowMetadata(Formatter):
     is_shadow_data: bool
 
     """Only available for the VIDEO and AUDIO data_type"""
-    frames_per_second: Optional[int]
+    frames_per_second: Optional[float]
     number_of_frames: int
     duration: Optional[float]
 
@@ -364,3 +366,93 @@ class LabelValidationState(BaseDTO):
     version: int
     is_valid: bool
     errors: List[str]
+
+
+class WorkflowGraphNodeDTO(BaseDTO):
+    uuid: str
+    title: str
+
+
+class LabelRowMetadataDTO(BaseDTO):
+    """
+    Contains helpful information about a label row.
+    """
+
+    label_hash: Optional[str] = Field(default=None, alias="label_uuid")
+    """Only present if the label row is initiated"""
+    created_at: Optional[datetime.datetime] = None
+    """Only present if the label row is initiated"""
+    last_edited_at: Optional[datetime.datetime] = None
+    """Only present if the label row is initiated"""
+    branch_name: str
+    """Only present if the label row is initiated or branch_name is set specifically"""
+
+    data_hash: str = Field(alias="data_uuid")
+    dataset_hash: str = Field(alias="dataset_uuid")
+    dataset_title: str
+    data_title: str
+    data_type: str
+    data_link: Optional[str] = None
+    """Can be `None` for label rows of image groups or DICOM series."""
+    label_status: LabelStatus
+    """Can be `None` for TMS2 projects"""
+    annotation_task_status: Optional[AnnotationTaskStatus] = None
+    """Only available for TMS2 project"""
+    workflow_graph_node: Optional[WorkflowGraphNode] = None
+    is_shadow_data: bool = False
+
+    """Only available for the VIDEO and AUDIO data_type"""
+    frames_per_second: Optional[float]
+    number_of_frames: int
+    duration: Optional[float]
+
+    """Only available for the VIDEO data_type"""
+    height: Optional[int]
+    width: Optional[int]
+
+    """Only available for the AUDIO data_type"""
+    audio_sample_rate: Optional[int]
+    audio_codec: Optional[str]
+    audio_bit_depth: Optional[int]
+    audio_num_channels: Optional[int]
+
+    priority: Optional[float] = None
+    """Only available for not complete tasks"""
+    client_metadata: Optional[dict] = None
+    images_data: Optional[list] = None
+    file_type: Optional[str] = None
+    """Only available for certain read requests"""
+    is_valid: bool = True
+
+
+def label_row_metadata_dto_to_label_row_metadata(label_row_metadata_dto: LabelRowMetadataDTO) -> LabelRowMetadata:
+    return LabelRowMetadata(
+        label_hash=label_row_metadata_dto.label_hash,
+        created_at=label_row_metadata_dto.created_at,
+        last_edited_at=label_row_metadata_dto.last_edited_at,
+        data_hash=label_row_metadata_dto.data_hash,
+        dataset_hash=label_row_metadata_dto.dataset_hash,
+        dataset_title=label_row_metadata_dto.dataset_title,
+        data_title=label_row_metadata_dto.data_title,
+        data_type=label_row_metadata_dto.data_type,
+        data_link=label_row_metadata_dto.data_link,
+        label_status=label_row_metadata_dto.label_status,
+        annotation_task_status=label_row_metadata_dto.annotation_task_status,
+        workflow_graph_node=label_row_metadata_dto.workflow_graph_node,
+        is_shadow_data=label_row_metadata_dto.is_shadow_data,
+        number_of_frames=label_row_metadata_dto.number_of_frames,
+        duration=label_row_metadata_dto.duration,
+        audio_sample_rate=label_row_metadata_dto.audio_sample_rate,
+        audio_codec=label_row_metadata_dto.audio_codec,
+        audio_bit_depth=label_row_metadata_dto.audio_bit_depth,
+        audio_num_channels=label_row_metadata_dto.audio_num_channels,
+        frames_per_second=label_row_metadata_dto.frames_per_second,
+        height=label_row_metadata_dto.height,
+        width=label_row_metadata_dto.width,
+        priority=label_row_metadata_dto.priority,
+        client_metadata=label_row_metadata_dto.client_metadata,
+        images_data=label_row_metadata_dto.images_data,
+        file_type=label_row_metadata_dto.file_type,
+        is_valid=label_row_metadata_dto.is_valid,
+        branch_name=label_row_metadata_dto.branch_name,
+    )
