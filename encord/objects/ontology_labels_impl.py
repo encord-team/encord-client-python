50f9e6c7c8a31c394879acd9c384800fa749f6af
james-encord <james@encord.com>
2024-12-03 18:39:54 +0000

    fix: save, missing featureHash

--- encord/objects/ontology_labels_impl.py
+++ encord/objects/ontology_labels_impl.py
@@ -1638,6 +1638,7 @@ class LabelRowV2:
                 ret[obj.object_hash]["lastEditedBy"] = annotation.last_edited_by
                 ret[obj.object_hash]["lastEditedAt"] = annotation.last_edited_at.strftime(DATETIME_LONG_STRING_FORMAT)
                 ret[obj.object_hash]["manualAnnotation"] = annotation.manual_annotation
+                ret[obj.object_hash]["featureHash"] = obj.feature_hash
 
         return ret
 
