b79d8ae2eea0c6b82b06274f634f613d36a67913
Arthur Alglave <arthur@encord.com>
2025-01-02 17:23:42 +0000

    remove unused code, BaseDTO works fine

--- tests/objects/test_coordinates.py
+++ tests/objects/test_coordinates.py
@@ -1,7 +1,7 @@
 from copy import copy
 
 from encord.objects import Shape
-from encord.objects.coordinates import ACCEPTABLE_COORDINATES_FOR_ONTOLOGY_ITEMS, SkeletonCoordinate, Visibility
+from encord.objects.coordinates import ACCEPTABLE_COORDINATES_FOR_ONTOLOGY_ITEMS
 
 
 def test_acceptable_coordinates_for_ontology_items() -> None:
@@ -10,30 +10,3 @@ def test_acceptable_coordinates_for_ontology_items() -> None:
         assert shape in all_mappings
         del all_mappings[shape]
     assert not all_mappings
-
-
-def test_skeleton_coordinates_without_visibility_works() -> None:
-    invisible_pnt = SkeletonCoordinate.from_dict(
-        {
-            "x": 0.2356,
-            "y": 0.9396,
-            "name": "point_2",
-            "color": "#000000",
-            "value": "point_2",
-            "featureHash": "OqR+F4dN",
-            "visibility": "invisible",
-        }
-    )
-    assert invisible_pnt.visibility is Visibility.INVISIBLE
-
-    visible_pnt = SkeletonCoordinate.from_dict(
-        {
-            "x": 0.2356,
-            "y": 0.9396,
-            "name": "point_2",
-            "color": "#000000",
-            "value": "point_2",
-            "featureHash": "OqR+F4dN",
-        }
-    )
-    assert visible_pnt.visibility is Visibility.VISIBLE
