c5e0d53d4f8a125846bf264034e0e497cd3fe188
Jim Broadbent <jim@encord.com>
2025-02-26 16:15:53 +0000

    fix: Allow pathway de-serialisation in a Pydantic V1/V2 compatible manner
    Instead of attempting to use the function based discrimiantor, if we have more discerning classes,
    this can be done.
    
    Include a test. More required

--- tests/workflow/test_project_workflow.py
+++ tests/workflow/test_project_workflow.py
@@ -1,4 +1,27 @@
+from typing import Any, Dict
+
+from encord.orm.workflow import WorkflowAgentNode, WorkflowDTO
 from encord.workflow import Workflow
+
+raw_agent_workflow: Dict[str, Any] = {
+    "stages": [
+        {
+            "stageType": "AGENT",
+            "uuid": "9d6edee0-ba3e-423c-8c96-0140b07c93ed",
+            "title": "Pre-labeler",
+            "pathways": [
+                {
+                    "uuid": "ae956722-561f-4586-b6d6-145e2f64b18a",
+                    "title": "Pre-labelling",
+                    "destinationUuid": "1252fdb8-93cb-41a2-b341-b83ab97b5277",
+                }
+            ],
+        },
+        {"stageType": "ANNOTATION", "uuid": "1252fdb8-93cb-41a2-b341-b83ab97b5277", "title": "Annotate"},
+        {"stageType": "REVIEW", "uuid": "8cac6636-2ef1-4bbe-848a-b5ffee158c34", "title": "Review"},
+        {"stageType": "DONE", "uuid": "7e7598de-612c-40c4-ba08-5dfec8c3ae8f", "title": "Complete"},
+    ]
+}
 
 
 def test_project_workflow_get_stage(workflow: Workflow) -> None:
@@ -8,3 +31,11 @@ def test_project_workflow_get_stage(workflow: Workflow) -> None:
     assert workflow.get_stage(uuid=review_stage_uuid).title == "Review 1"
     assert workflow.get_stage(uuid=str(review_stage_uuid)).title == "Review 1"
     assert workflow.get_stage(name="Review 1").uuid == review_stage_uuid
+
+
+def test_agent_project_with_pathway_serialisation_deserialisation() -> None:
+    workflow = WorkflowDTO.from_dict(raw_agent_workflow)
+    assert workflow.stages
+    agent_stage = workflow.stages[0]
+    assert isinstance(agent_stage, WorkflowAgentNode)
+    assert agent_stage.pathways
