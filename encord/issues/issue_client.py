04f011c0bdb13678c984647224d735861e082657
Ricky Kawagishi <ricky@encord.com>
2025-05-09 17:15:18 +0100

    clean

--- encord/issues/issue_client.py
+++ encord/issues/issue_client.py
@@ -17,32 +17,31 @@ class _IssueAnchorType(CamelStrEnum):
 
 
 @final
-class _ProjectDataUnitIssueAnchor(BaseDTO):
+class _FileIssueAnchor(BaseDTO):
     type: Literal[_IssueAnchorType.DATA_UNIT] = _IssueAnchorType.DATA_UNIT
     project_hash: UUID
     data_hash: UUID
 
 
 @final
-class _ProjectDataUnitFrameIssueAnchor(BaseDTO):
+class _FrameIssueAnchor(BaseDTO):
     type: Literal[_IssueAnchorType.FRAME] = _IssueAnchorType.FRAME
     project_hash: UUID
     data_hash: UUID
+    frame_index: int
 
 
 @final
-class _ProjectDataUnitFrameCoordinateIssueAnchor(BaseDTO):
+class _CoordinateIssueAnchor(BaseDTO):
     type: Literal[_IssueAnchorType.FRAME_COORDINATE] = _IssueAnchorType.FRAME_COORDINATE
+    project_hash: UUID
+    data_hash: UUID
     frame_index: int
     x: float
     y: float
-    project_hash: UUID
-    data_hash: UUID
 
 
-_IssueAnchor = Union[
-    _ProjectDataUnitIssueAnchor, _ProjectDataUnitFrameIssueAnchor, _ProjectDataUnitFrameCoordinateIssueAnchor
-]
+_IssueAnchor = Union[_FileIssueAnchor, _FrameIssueAnchor, _CoordinateIssueAnchor]
 
 
 @final
@@ -57,26 +56,24 @@ class _CreateIssuesPayload(BaseDTO):
 
 
 @dataclass
-class _IssueClient:
+class TaskIssueClient:
     api_client: ApiClient
+    project_hash: UUID
+    data_hash: UUID
 
-    def add_file_issue(self, *, project_hash: UUID, data_hash: UUID, comment: str, issue_tags: List[str]):
+    def add_file_issue(self, comment: str, issue_tags: List[str]):
         self._add_issue(
-            anchor=_ProjectDataUnitIssueAnchor(
-                project_hash=project_hash,
-                data_hash=data_hash,
+            anchor=_FileIssueAnchor(
+                project_hash=self.project_hash,
+                data_hash=self.data_hash,
             ),
             comment=comment,
             issue_tags=issue_tags,
         )
 
-    def add_frame_issue(
-        self, *, project_hash: UUID, data_hash: UUID, frame_index: int, comment: str, issue_tags: List[str]
-    ):
+    def add_frame_issue(self, frame_index: int, comment: str, issue_tags: List[str]):
         self._add_issue(
-            anchor=_ProjectDataUnitFrameIssueAnchor(
-                project_hash=project_hash, data_hash=data_hash, frame_index=frame_index
-            ),
+            anchor=_FrameIssueAnchor(project_hash=self.project_hash, data_hash=self.data_hash, frame_index=frame_index),
             comment=comment,
             issue_tags=issue_tags,
         )
@@ -95,45 +92,4 @@ class _IssueClient:
                 ]
             ),
             result_type=None,
-        )
-
-
-class TaskIssueClient:
-    def __init__(self, api_client: ApiClient, project_hash: UUID, data_hash: UUID):
-        self._project_hash = project_hash
-        self._data_hash = data_hash
-        self._client = _IssueClient(api_client)
-
-    def add_file_issue(self, comment: str, issue_tags: List[str]):
-        self._client.add_file_issue(
-            project_hash=self._project_hash, data_hash=self._data_hash, comment=comment, issue_tags=issue_tags
-        )
-
-    def add_frame_issue(self, frame_index: int, comment: str, issue_tags: List[str]):
-        self._client.add_frame_issue(
-            project_hash=self._project_hash,
-            data_hash=self._data_hash,
-            frame_index=frame_index,
-            comment=comment,
-            issue_tags=issue_tags,
-        )
-
-
-class ProjectIssueClient:
-    def __init__(self, api_client: ApiClient, project_hash: UUID):
-        self._project_hash = project_hash
-        self._client = _IssueClient(api_client)
-
-    def add_file_issue(self, data_hash: UUID, comment: str, issue_tags: List[str]):
-        self._client.add_file_issue(
-            project_hash=self._project_hash, data_hash=data_hash, comment=comment, issue_tags=issue_tags
-        )
-
-    def add_frame_issue(self, data_hash: UUID, frame_index: int, comment: str, issue_tags: List[str]):
-        self._client.add_frame_issue(
-            project_hash=self._project_hash,
-            data_hash=data_hash,
-            frame_index=frame_index,
-            comment=comment,
-            issue_tags=issue_tags,
         )
