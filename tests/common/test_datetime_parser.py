3bd7fd1ac4b103ef39d39cf769897060ac183ce0
Eloy Pérez Torres <eloy@encord.com>
2025-05-20 05:58:26 -1000

    chore: add tests using naive and aware datetimes

--- tests/common/test_datetime_parser.py
+++ tests/common/test_datetime_parser.py
@@ -1,32 +1,49 @@
 from datetime import datetime, timedelta, timezone
 
+import pytest
+
 from encord.common.constants import DATETIME_LONG_STRING_FORMAT
 from encord.common.time_parser import parse_datetime
 
 
-def test_can_parse_iso_timestamp() -> None:
-    expected_datetime = datetime.now()
+@pytest.mark.parametrize(
+    "timezone_offset",
+    [-12, -7, -3, 0, 2, 5, 11],
+)
+def test_parse_datetime_timezone_aware_iso_string(timezone_offset) -> None:
+    tz = timezone(timedelta(hours=timezone_offset))
+    expected_datetime = datetime.now(tz=tz)
 
-    datetime_as_iso = expected_datetime.isoformat()
-    parsed_datetime = parse_datetime(datetime_as_iso)
+    datetime_as_iso_string = expected_datetime.isoformat()
+    parsed_datetime = parse_datetime(datetime_as_iso_string)
 
     assert parsed_datetime == expected_datetime
 
 
-def test_can_parse_encord_time_format() -> None:
-    original_datetime = datetime.now()
+def test_parse_datetime_timezone_naive_iso_string() -> None:
+    expected_datetime = datetime.now()
 
-    datetime_as_encord_string = original_datetime.strftime(DATETIME_LONG_STRING_FORMAT)
+    datetime_as_iso_string = expected_datetime.isoformat()
+    parsed_datetime = parse_datetime(datetime_as_iso_string)
+
+    assert parsed_datetime == expected_datetime.replace(tzinfo=timezone.utc)
+
+
+@pytest.mark.parametrize("timezone_name", ["", "UTC", "GMT"])
+def test_parse_datetime_encord_string(timezone_name: str) -> None:
+    expected_datetime = datetime.now()
+
+    datetime_as_encord_string = expected_datetime.strftime(DATETIME_LONG_STRING_FORMAT) + f"{timezone_name}"
     parsed_datetime = parse_datetime(datetime_as_encord_string)
 
     # Short string format doesn't include milliseconds, so dropping them
-    expected_datetime = original_datetime.replace(microsecond=0)
-    assert parsed_datetime == expected_datetime
+    assert parsed_datetime == expected_datetime.replace(microsecond=0, tzinfo=timezone.utc)
 
 
-def test_can_parse_encord_time_from() -> None:
-    original_datetime = "Thu Jan 11 2024 12:09:51 GMT+0600 (Bangladesh Standard Time)"
-    parsed_datetime = parse_datetime(original_datetime)
-
+def test_parse_datetime_custom_string() -> None:
+    expected_datetime_str = "Thu Jan 11 2024 12:09:51 GMT+0600 (Bangladesh Standard Time)"
     expected_datetime = datetime(2024, 1, 11, 12, 9, 51, tzinfo=timezone(timedelta(hours=-6)))
+
+    parsed_datetime = parse_datetime(expected_datetime_str)
+
     assert parsed_datetime == expected_datetime
