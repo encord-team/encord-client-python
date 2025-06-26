6cfdd35070fe889aaa274e15bf9ce1709637fecd
Sergei Chertkov <sergei@encord.com>
2025-06-26 17:08:46 +0100

    Fix error when getting timers information

--- encord/orm/analytics.py
+++ encord/orm/analytics.py
@@ -5,7 +5,7 @@ from uuid import UUID
 
 from encord.common.utils import snake_to_camel
 from encord.orm.base_dto import BaseDTO
-from encord.orm.workflow import WorkflowNode, WorkflowStageType
+from encord.orm.workflow import BaseWorkflowNode, WorkflowNode, WorkflowStageType
 from encord.utilities.project_user import ProjectUserRole
 
 
@@ -59,4 +59,4 @@ class TimeSpent(BaseDTO):
     dataset_uuid: UUID
     dataset_title: str
     workflow_task_uuid: Optional[UUID] = None
-    workflow_stage: WorkflowNode
+    workflow_stage: BaseWorkflowNode
