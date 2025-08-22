eb2cc4f85e6885efa7af506d1bf338c757e57500
Laverne-Encord <145356218+Laverne-Encord@users.noreply.github.com>
2025-08-22 12:04:33 +0100

    Updates SDK Doc Code Examples for Data Groups exporting labels

--- tests/docs/export_labels_consensus-labels.py
+++ tests/docs/export_labels_consensus-labels.py
@@ -24,7 +24,7 @@ project = user_client.get_project(PROJECT_ID)
 
 # Downloads a local copy of all the labels
 # Without the include_all_label_branches flag only the MAIN branch labels export
-label_rows = project.list_label_rows_v2(include_all_label_branches=True)
+label_rows = project.list_label_rows_v2(include_children=True, include_all_label_branches=True)
 
 output_data = []  # This will hold the output data to be saved as JSON
 
