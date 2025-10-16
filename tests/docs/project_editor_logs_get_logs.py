157bda10a8827f42692aaa2617df68f8e03d4d4e
solene-encord <solene@encord.com>
2025-10-16 13:13:21 +0200

    improving the documentation

--- tests/docs/project_editor_logs_get_logs.py
+++ tests/docs/project_editor_logs_get_logs.py
@@ -11,7 +11,9 @@ from encord import EncordUserClient
 SSH_PATH = "/Users/chris-encord/ssh-private-key.txt"
 PROJECT_ID = "00000000-0000-0000-0000-000000000000"
 DATA_UNIT_ID = "00000000-0000-0000-0000-000000000000"  # The unique identifier (data_hash) for the data unit
-
+WORKFLOW_STAGE_ID = "00000000-0000-0000-0000-000000000000" # The unique identifier for the workflow stage
+ACTION = "create_object"  # Example action
+ACTOR_USER_EMAIL = "some-email@gmail.com"
 
 # Create user client using SSH key
 user_client: EncordUserClient = EncordUserClient.create_with_ssh_private_key(
@@ -51,8 +53,8 @@ filtered_logs_response = project.get_editor_logs(
     start_time=start_time,
     end_time=end_time,
     limit=200,
-    action="create_object",
-    actor_user_email="some-email@gmail.com",
-    workflow_stage_id=UUID("some-stage-id"),
-    data_unit_id=UUID("some-data-unit-id")
+    action=ACTION,
+    actor_user_email=ACTOR_USER_EMAIL,
+    workflow_stage_id=UUID(WORKFLOW_STAGE_ID),
+    data_unit_id=UUID(DATA_UNIT_ID)
 )
