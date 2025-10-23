802579c90ccf1190dd340cf2491c3f53059df9d1
solene-encord <solene@encord.com>
2025-10-23 11:07:50 +0100

    fix: PLA-1817 - Adding back the page_token because otherwise it breaks editor logs (#990)

--- encord/project.py
+++ encord/project.py
@@ -594,8 +594,7 @@ class Project:
     ) -> Iterator[EditorLog]:
         """Get editor logs, represents the actions taken in the Editor UI.
 
-        The limit has a default and maximum value of 500.
-        The start_time and end_time parameters are required.
+        The start_time and end_time parameters are required. The maximum time range is 30 days.
 
         Args:
            action: Filter the editor logs by action.
