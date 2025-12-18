cd1760cacddb3997dd06b4aa73f38c5eb863b1ac
Abhishek Deora <abhishek@encord.com>
2025-12-18 15:54:20 +0000

    feat: metadata geospatial

--- encord/orm/metadata_schema.py
+++ encord/orm/metadata_schema.py
@@ -1,6 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class GeospatialCoordinates(BaseModel):
-    latitude: float
-    longitude: float
+    latitude: float = Field(..., ge=-90.0, le=90.0)
+    longitude: float = Field(..., ge=-180.0, le=180.0)
