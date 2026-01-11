266577a81b8e265da4a2e153f0b1e0f436e56c26
Clinton Wee <clinton@encord.com>
2026-01-11 21:03:22 +0000

    Reformatted some tests

--- tests/objects/spaces/test_get_space.py
+++ tests/objects/spaces/test_get_space.py
@@ -1,42 +1,71 @@
+from typing import get_args
 from unittest.mock import Mock
 
 import pytest
 
 from encord.exceptions import LabelRowError
 from encord.objects import LabelRowV2
+from encord.objects.ontology_labels_impl import SpaceLiteral
+from encord.utilities.type_utilities import exhaustive_guard
 from tests.objects.data.data_group.all_modalities import DATA_GROUP_METADATA
+
+
+def _get_space_id_from_space_literal(space_literal: SpaceLiteral) -> str:
+    if space_literal == "video":
+        return "video-uuid"
+    elif space_literal == "image":
+        return "image-uuid"
+    elif space_literal == "image_sequence":
+        return "image-sequence-uuid"
+    elif space_literal == "audio":
+        return "audio-uuid"
+    elif space_literal == "text":
+        return "text-uuid"
+    elif space_literal == "html":
+        return "html-uuid"
+    elif space_literal == "medical_file":
+        return "dicom-uuid"
+    elif space_literal == "medical_stack":
+        return "dicom-stack-uuid"
+    else:
+        exhaustive_guard(space_literal, message=f"Missing implementation for space {space_literal}")
+
+
+def _get_space_layout_key_from_space_literal(space_literal: SpaceLiteral) -> str:
+    if space_literal == "video":
+        return "main-video"
+    elif space_literal == "image":
+        return "main-image"
+    elif space_literal == "image_sequence":
+        return "main-image-sequence"
+    elif space_literal == "audio":
+        return "main-audio"
+    elif space_literal == "text":
+        return "main-text"
+    elif space_literal == "html":
+        return "main-html"
+    elif space_literal == "medical_file":
+        return "left-shoulder"
+    elif space_literal == "medical_stack":
+        return "xray-stack"
+    else:
+        exhaustive_guard(space_literal, message=f"Missing implementation for space {space_literal}")
 
 
 def test_get_space_by_id(ontology):
     label_row = LabelRowV2(DATA_GROUP_METADATA, Mock(), ontology)
-
-    video_space = label_row._get_space(id="video-uuid", type_="video")
-    assert video_space.space_id == "video-uuid"
-
-    image_space = label_row._get_space(id="image-uuid", type_="image")
-    assert image_space.space_id == "image-uuid"
-
-    text_space = label_row._get_space(id="text-uuid", type_="text")
-    assert text_space.space_id == "text-uuid"
-
-    audio_space = label_row._get_space(id="audio-uuid", type_="audio")
-    assert audio_space.space_id == "audio-uuid"
+    for space_literal in get_args(SpaceLiteral):
+        space_id = _get_space_id_from_space_literal(space_literal)
+        space = label_row._get_space(id=space_id, type_=space_literal)
+        assert space.space_id == space_id
 
 
 def test_get_space_by_layout_key(ontology):
     label_row = LabelRowV2(DATA_GROUP_METADATA, Mock(), ontology)
-
-    video_space = label_row._get_space(layout_key="main-video", type_="video")
-    assert video_space.space_id == "video-uuid"
-
-    image_space = label_row._get_space(layout_key="main-image", type_="image")
-    assert image_space.space_id == "image-uuid"
-
-    text_space = label_row._get_space(layout_key="main-text", type_="text")
-    assert text_space.space_id == "text-uuid"
-
-    audio_space = label_row._get_space(layout_key="main-audio", type_="audio")
-    assert audio_space.space_id == "audio-uuid"
+    for space_literal in get_args(SpaceLiteral):
+        space_layout_key = _get_space_layout_key_from_space_literal(space_literal)
+        space = label_row._get_space(layout_key=space_layout_key, type_=space_literal)
+        assert space is not None
 
 
 def test_get_space_that_do_not_exist(ontology):
